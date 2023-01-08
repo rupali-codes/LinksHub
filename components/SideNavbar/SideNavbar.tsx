6643ce9162feecec8fe64f62c4db35c43181e3f5
Rupali Haldiya <78981177+rupali-codes@users.noreply.github.com>
2023-01-08 19:10:14 +0530

    Develop > main (#6)
    * feat: made dynamic sidebar and removed unused code
    
    * split into different components (#5)
    
    * feat: split into different components
    
    * feat: sidebar element updated
    
    Co-authored-by: rohitdasu <dasurohit123@gmail.com>
    Co-authored-by: Rohit <48400770+rohitdasu@users.noreply.github.com>

---
+++ components/SideNavbar/SideNavbar.tsx
@@ -0,0 +1,74 @@
+import classNames from "classnames";
+import React, { useState } from "react";
+import { data, sidebarData } from "../../database/data";
+import { SideNavbarElement } from "./SideNavbarElement";
+
+export const SideNavbar = () => {
+  const [isSidebarActive, setIsSidebarActive] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarActive((prev) => !prev);
+  };
+
+  return (
+    <div className={`lg:w-1/5 bg-base-300 p-4 lg:min-h-screen`}>
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold mb-4">
+          <span className="border-b-4 border-violet-500 text-violet-200">
+            Links
+          </span>
+          <span className="text-violet-500">Hub</span>
+        </h1>
+
+        <label className="btn btn-circle swap swap-rotate lg:hidden">
+          <input
+            type="checkbox"
+            onClick={toggleSidebar}
+            className="lg:hidden"
+          />
+
+          <svg
+            className="swap-off fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 512 512"
+          >
+            <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+          </svg>
+
+          <svg
+            className="swap-on fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 512 512"
+          >
+            <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+          </svg>
+        </label>
+      </div>
+      <div
+        className={classNames(
+          "lg:block transition-all ease-in duration-300",
+          isSidebarActive ? "block" : "hidden"
+        )}
+      >
+        <div className="py-4 flex flex-col justify-center gap-8">
+          {sidebarData.map((item, index) => {
+            return (
+              <div key={index}>
+                <h2 key={index} className="uppercase font-bold text-xl">
+                  {item.category}
+                </h2>
+                {item.subcategory.map((s, i) => {
+                  return <SideNavbarElement key={i} title={s} />;
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
--- types/index.ts
+++ types/index.ts
@@ -1,28 +1,27 @@
-export type dataList = {
-  name:string,
-  description:string,
-  url: string
-}
+export type DataList = {
+  name: string;
+  description: string;
+  url: string;
+};
 
-export interface ICollapsProps {
-  title:string
-  elements:dataList[]
+export interface ISideNavbarElement {
+  title: string;
 }
 
 export interface IData {
-  id: string,
-  name: string,
-  description: string,
-  url: string,
-  category: Category,
-  subcategory: SubCategory
+  id: string;
+  name: string;
+  description: string;
+  url: string;
+  category: Category;
+  subcategory: SubCategory;
 }
 
 export type Category = "frontend" | "backend";
 
-export type SubCategory = "images" | "illustrations" | "colors" | "fonts"
+export type SubCategory = "images" | "illustrations" | "colors" | "fonts";
 
 export interface ISidebar {
-    category: Category,
-    subcategory: Array<string>
+  category: Category;
+  subcategory: Array<string>;
 }
