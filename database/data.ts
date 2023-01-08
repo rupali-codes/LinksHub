33f07b059b2ddfa8c70eb91c797fa282dc46ec9d
rupali-codes <rupali7487@gmail.com>
2023-01-08 15:34:28 +0530

    structuring db

---
+++ database/data.ts
@@ -0,0 +1,40 @@
+import { IData, ISidebar } from "../types";
+
+export const data: IData[] = [
+  {
+    id: "1",
+    name: "unDraw",
+    description: "unDraw is a site to get free illustrations",
+    url: "https://undraw.io/illustrations",
+    category: "frontend",
+    subcategory: "illustrations",
+  },
+  {
+    id: "2",
+    name: "Unsplash",
+    description: "Unsplash is a site to get free images",
+    url: "https://unsplash.com",
+    category: "frontend",
+    subcategory: "images",
+  },
+  {
+    id: "3",
+    name: "dummyimage",
+    description:
+      "dummyimage is a site to get dummy image with free hand sizing",
+    url: "https://dummyimage.com/",
+    category: "frontend",
+    subcategory: "images",
+  },
+];
+
+export const sidebar: ISidebar[] = [
+  {
+    category: "frontend",
+    subcategory: ["images", "fonts", "colors", "illustrations", "animations"],
+  },
+  {
+    category: "backend",
+    subcategory: ["validation", "security", "authentication", "caching"],
+  },
+];
