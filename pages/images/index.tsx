5d6d1a0103d72550f86c3cdd068e0f3b95455b0d
Eugenics <eugeneishado44@gmail.com>
2023-01-17 16:05:28 +0100

    fix(sidebar & coming-soon & context): fixed closing of side nav, setting up context and pulse animation on text

--- pages/images/index.tsx
+++ pages/images/index.tsx
@@ -6,7 +6,7 @@ import { IData } from '../../types'
 
 const Images = () => {
   return (
-    <section className=''>
+    <section className='md:mt-0 mt-20 md:p-0 p-5'>
     <div className="flex itmes-center text-white text-3xl my-4 font-bold uppercase">
           <FaSlackHash className="mt-1 text-gray-400" />
           <h2 className="">Images</h2>
