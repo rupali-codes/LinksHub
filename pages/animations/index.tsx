511549c7589d1a4be376d03eae0a34e68bd0de3b
Eugene44-hub <75457464+Eugene44-hub@users.noreply.github.com>
2023-01-18 11:07:02 +0100

    chroe: filtering data array
    * fix(Sidebar):Made Sidebar Scrollable on mobile
    
    * feat(Cards):Made reusable card component to filter through database and display contents
    
    * feat(Cards):Made reusable card component to filter through database and display contents
    
    * feat(Cards):Made reusable card component to filter through database and display contents
    
    * feat(Cards):Made reusable card component to filter through database and display contents

--- pages/animations/index.tsx
+++ pages/animations/index.tsx
@@ -1,24 +1,21 @@
-import React, { useContext } from 'react'
-import { FaSlackHash } from 'react-icons/fa'
-import { GlobalContext } from '../../context/GlobalContext'
+import React, { useContext } from "react";
+import { FaSlackHash } from "react-icons/fa";
+import Cards from "../../components/cards";
+
+
+;
 
 const Animations = () => {
-  const {sidebar}=useContext(GlobalContext)
-
   return (
-   <section className='md:mt-0 mt-20 md:p-0 p-5'>
+    <section className="md:mt-0 mt-20 lg:p-0 pt-10 px-5">
 
-    <div className="text-3xl text-white flex font-bold">
+      <div className="text-3xl text-white flex font-bold">
+        <FaSlackHash className="mt-1 mr-2 text-gray-400" />
+        Animations
+      </div>
+      <Cards />
+    </section>
+  );
+};
 
-      <FaSlackHash className="mt-1 mr-2 text-gray-400" />
-      Animations
-    </div>
-    <p className={`text-white mt-5 z-[-10] text-2xl font-bold  ${!sidebar&&'animate-pulse'}`}>
-
-      <span className="text-purple-500">Coming</span> Soon
-    </p>
-  </section>
-  )
-}
-
-export default Animations
+export default Animations;
