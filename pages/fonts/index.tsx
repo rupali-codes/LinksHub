549c6ea0143b3e021346cd39e33c16a4ba73b59c
Rupali Haldiya <78981177+rupali-codes@users.noreply.github.com>
2023-01-18 16:22:54 +0530

    fix: adding top margin (#35)

--- pages/fonts/index.tsx
+++ pages/fonts/index.tsx
@@ -5,7 +5,7 @@ import Cards from "../../components/cards";
 const Fonts = () => {
   return (
     <section className="md:mt-0 mt-20 lg:p-0 md:pt-20 pt-10 px-5">
-      <div className="text-3xl text-white flex font-bold">
+      <div className="flex itmes-center text-white text-3xl my-4 font-bold uppercase">
         <FaSlackHash className="mt-1 mr-2 text-gray-400" />
         Fonts
       </div>
