2396b37628b16f6c71f54b240fc78e3eb751db71
Eugenics <eugeneishado44@gmail.com>
2023-01-18 10:54:13 +0100

    feat(Cards):Made reusable card component to filter through database and display contents

--- pages/images/index.tsx
+++ pages/images/index.tsx
@@ -7,7 +7,7 @@ import { IData } from "../../types";
 
 const Images = () => {
   return (
-    <section className="md:mt-0 mt-20 md:p-0 p-5">
+    <section className="md:mt-0 mt-20 lg:p-0 pt-10 px-5">
       <div className="flex itmes-center text-white text-3xl my-4 font-bold uppercase">
         <FaSlackHash className="mt-1 text-gray-400" />
         <h2 className="">Images</h2>
