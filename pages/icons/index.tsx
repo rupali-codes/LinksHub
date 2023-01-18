3c5c8bc899e46bbedfedd4e82307ca12ae6ce074
Rupali Haldiya <rupali7487@gmail.com>
2023-01-18 16:19:44 +0530

    fix:adding top margin

--- pages/icons/index.tsx
+++ pages/icons/index.tsx
@@ -5,9 +5,9 @@ import Cards from "../../components/cards";
 const Icons = () => {
   return (
     <section className="md:mt-0 mt-20 md:p-0 p-5">
-      <div className="text-3xl text-white flex font-bold">
+      <div className="flex itmes-center text-white text-3xl my-4 font-bold uppercase">
         <FaSlackHash className="mt-1 mr-2 text-gray-400" />
-        Icons
+          Icons
       </div>
       <Cards />
     </section>
--- pages/images/index.tsx
+++ pages/images/index.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { FaSlackHash } from "react-icons/fa";
 import Cards from "../../components/cards";
-import { LinkContainer } from "../../components/LinksConatiners/LinkContainer";
-import { data } from "../../database/data";
-import { IData } from "../../types";
 
 const Images = () => {
   return (
     <section className="md:mt-0 mt-20 lg:p-0 pt-10 px-5">
       <div className="flex itmes-center text-white text-3xl my-4 font-bold uppercase">
-        <FaSlackHash className="mt-1 text-gray-400" />
-        <h2 className="">Images</h2>
+        <FaSlackHash className="mt-1 mr-2 text-gray-400" />
+          Images
       </div>
 
       <Cards />
