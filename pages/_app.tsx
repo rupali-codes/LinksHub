2e6e31a6a7e389c2f54666bf498cd12bfa7a47db
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2023-01-18 14:12:26 +0100

    [Feat :rocket:] - create a spinner and preloader component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,24 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import GeneralLayout from "../layouts/GeneralLayout";
 import { GlobalProvider } from "../context/GlobalContext";
+import { Preloader } from "../components/Loader/Preloader";
+import { useEffect, useState } from "react";
+import { Spinner } from "../components/Loader/Spinner";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [loading, setSpinner] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setSpinner(false), 1000)
+  });
   return (
     <GlobalProvider>
-      <GeneralLayout>
-        <Component {...pageProps} />
-      </GeneralLayout>
+      {!loading ? 
+        (<GeneralLayout>
+          <Component {...pageProps} />
+        </GeneralLayout>) :
+        (<Preloader backgroundColor="bg-violet-700" spinnerColor="#8b5cf6" spinnerSize="40px" />)
+      }
     </GlobalProvider>
   );
 }
