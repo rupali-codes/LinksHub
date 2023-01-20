cf949078b3b8eb65f822622ac41c18ed2b9acc03
gangadharbenni <gangadharbenni@outlook.com>
2023-01-20 19:48:45 +0530

    align toggler

--- pages/testing/index.tsx
+++ pages/testing/index.tsx
@@ -4,7 +4,7 @@ import Cards from "../../components/cards";
 
 const Testing = () => {
   return (
-    <section className="md:mt-0 mt-20 lg:p-0 pt-10 px-5">
+    <section className="md:mt-0 lg:p-0 pt-10 px-5">
       <div className="flex itmes-center text-3xl my-4 font-bold uppercase dark:text-gray-300">
         <FaSlackHash className="mt-1 mr-2 text-gray-400" />
         Testing
