0560faa2ecce907fa2e5d93dd256e947d8a5e1c3
rohitdasu <dasurohit123@gmail.com>
2023-01-22 14:41:23 +0530

    wip

--- pages/[subcategory].tsx
+++ pages/[subcategory].tsx
@@ -1,17 +1,20 @@
-import React, { useContext } from "react";
+import { useRouter } from "next/router";
+import React from "react";
 import { FaSlackHash } from "react-icons/fa";
-import Cards from "../../components/cards";
+import Cards from "../components/cards";
 
-const Animations = () => {
+const SubCategory = () => {
+  const router = useRouter();
+  const category = router.asPath.replace("/", "");
   return (
     <section className="md:mt-0 lg:p-0 pt-10 px-5">
       <div className="flex items-center text-3xl my-4 font-bold uppercase dark:text-gray-300">
         <FaSlackHash className="mt-1 mr-2 text-gray-600 dark:text-gray-300" />
-        <span className="text-gray-900 dark:text-gray-100">Animations</span>
+        <span className="text-gray-900 dark:text-gray-100">{category}</span>
       </div>
       <Cards />
     </section>
   );
 };
 
-export default Animations;
+export default SubCategory;
