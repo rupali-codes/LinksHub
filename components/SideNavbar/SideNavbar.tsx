a9fe587dbc5cb96ceba207a26c3dff3be959da04
Leeyah-123 <junaidaaliyah260@gmail.com>
2023-01-23 19:48:30 +0100

    chore: Make navbar close on nav-link click or body click

--- components/SideNavbar/SideNavbar.tsx
+++ components/SideNavbar/SideNavbar.tsx
@@ -1,26 +1,49 @@
-import classNames from "classnames";
-import React, { useContext, useState } from "react";
-import { data, sidebarData } from "../../database/data";
-import { SideNavbarElement } from "./SideNavbarElement";
-import { IconContext } from "react-icons";
-import { FaGithub } from "react-icons/fa";
-import Logo from "../logo";
-import { GlobalContext } from "../../context/GlobalContext";
-import Link from "next/link";
-import {AiOutlineMenu,AiOutlineClose} from 'react-icons/ai'
+import Link from 'next/link';
+import { createRef, useContext, useEffect } from 'react';
+import { IconContext } from 'react-icons';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import { FaGithub } from 'react-icons/fa';
+import { GlobalContext } from '../../context/GlobalContext';
+import { sidebarData } from '../../database/data';
+import Logo from '../logo';
+import { SideNavbarElement } from './SideNavbarElement';
 export const SideNavbar = () => {
-  const {toggleNav,sidebar,openNav,closeNav} = useContext(GlobalContext);
+  const { toggleNav, sidebar, openNav, closeNav } = useContext(GlobalContext);
+
+  const menuRef = createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    const handler = (e: MouseEvent): void => {
+      const target = e.target as HTMLElement;
+
+      if (!menuRef.current?.contains(target)) closeNav && closeNav();
+    };
+
+    document.addEventListener('mousedown', handler);
+
+    return () => {
+      document.removeEventListener('mousedown', handler);
+    };
+  });
 
   return (
     <div className={`lg:w-[290px] fixed top-0 left-0 w-full  lg:h-full`}>
       <div className="flex bg-base-300 p-4 justify-between dark:bg-gray-900">
         <Link href={'/'}>
-        <Logo className="text-3xl mb-4" />
+          <Logo className="text-3xl mb-4" />
         </Link>
         <ul className="inline-flex space-x-2">
           <li>
-            <a title="Link to Github project (External Link)" className="dark:text-gray-300" target="_blank" rel="noopener noreferrer" href="https://github.com/rupali-codes/LinksHub">
-              <IconContext.Provider value={{ className: "shared-class", size: "28" }}>
+            <a
+              title="Link to Github project (External Link)"
+              className="dark:text-gray-300"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/rupali-codes/LinksHub"
+            >
+              <IconContext.Provider
+                value={{ className: 'shared-class', size: '28' }}
+              >
                 <FaGithub />
               </IconContext.Provider>
             </a>
@@ -29,29 +52,30 @@ export const SideNavbar = () => {
 
         <button
           className="relative h-[24px] w-[24px] lg:hidden dark:text-gray-300"
-          onClick={toggleNav&&toggleNav}
+          onClick={toggleNav && toggleNav}
         >
           <AiOutlineMenu
             size={24}
             className={`transition-all left-0 top-[5%] duration-[.5s] absolute ${
-              sidebar ? "z-[-1] opacity-[0]" : "opacity-[1] z-[1]"
+              sidebar ? 'z-[-1] opacity-[0]' : 'opacity-[1] z-[1]'
             }`}
           />
           <AiOutlineClose
             size={24}
             className={`absolute left-0 top-[5%] transition-all duration-[1s] ${
               sidebar
-                ? "opacity-[1] z-[1]"
-                : "opacity-[0] rotate-[180deg] z-[-1]"
+                ? 'opacity-[1] z-[1]'
+                : 'opacity-[0] rotate-[180deg] z-[-1]'
             }`}
           />
         </button>
       </div>
       <div
-      
-        className={
-         `lg:translate-x-0 lg:w-full w-[75%] p-4 bg-base-300 transition-all whitespace-nowrap ease-in duration-300 overflow-x-hidden h-screen
-         ${sidebar?"translate-x-[0%] ":"translate-x-[-100%]"}  scrollColor z-[10] dark:bg-gray-900 dark:text-gray-300`}
+        ref={menuRef}
+        className={`lg:translate-x-0 lg:w-full w-[75%] p-4 bg-base-300 transition-all whitespace-nowrap ease-in duration-300 overflow-x-hidden h-screen
+         ${
+           sidebar ? 'translate-x-[0%] ' : 'translate-x-[-100%]'
+         }  scrollColor z-[10] dark:bg-gray-900 dark:text-gray-300`}
       >
         <div className=" flex flex-col justify-center gap-8 ">
           {sidebarData.map((item, index) => {
