e3e393e9dd322280836b7ac97b5d54ae2100e24f
Rupali Haldiya <rupali7487@gmail.com>
2023-01-26 01:15:41 +0530

    fix: color contrast

--- layouts/GeneralLayout.tsx
+++ layouts/GeneralLayout.tsx
@@ -5,7 +5,7 @@ const GeneralLayout = ({ children }: { children: ReactNode }) => {
   return (
     <section className="bg-gray-100 dark:bg-[#101623]">
       <SideNavbar />
-      <div className="lg:pl-[290px] md:h-screen mx-auto lg:ml-20 mt-20 lg:mt-0">
+      <div className="lg:pl-[290px] md:min-h-screen mx-auto lg:ml-20 mt-20 lg:mt-0 md:pb-8">
         {children}
       </div>
     </section>
