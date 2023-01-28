327b7c3787ad20424de90c03ae8a553f84e949d1
Connor <70613517+ConradQQ@users.noreply.github.com>
2023-01-28 13:14:17 +0100

    fix: update site title when clicked on Side navbar elements (#109)

--- pages/[subcategory].tsx
+++ pages/[subcategory].tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Cards from "../components/cards";
 import { TopBar } from "../components/TopBar/TopBar";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 const SubCategory = () => {
+  const router = useRouter();
+  const title :any = `LinksHub - ${router.asPath.charAt(1).toUpperCase()}${router.asPath.slice(2)}`;
   return (
     <section className="px-4">
+      <Head>
+        <title>{title}</title>
+      </Head>
       <TopBar />
       <Cards />
     </section>
