6c944311ded09837bc39d6a457f3bffec9887759
Francisco Cerdán <fcerdanguerrero@gmail.com>
2023-02-04 15:31:33 +0100

    Added search bar for sidebar. Created two search hooks:   -  useSearch: Manages search input and allows to use debounced search   -  useSidebarSearch: Uses previous hook to get data from database and      filter it if needed closes #114

---
+++ components/Searchbar/Searchbar.tsx
@@ -0,0 +1,40 @@
+interface SearchbarProps {
+  setSearch: (search: string) => void;
+}
+
+export const Searchbar = ({ setSearch }: SearchbarProps) => {
+  const handleSearch = (e: any) => {
+    setSearch(e.target.value);
+  };
+
+  return (
+    <>
+      <label className="sr-only">Search</label>
+      <div className="relative w-full mb-1.5">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-violet-500 dark:text-violet-500"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </div>
+        <input
+          type="text"
+          id="simple-search"
+          className="bg-transparent border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Search"
+          onChange={handleSearch}
+          required
+        />
+      </div>
+    </>
+  );
+};
