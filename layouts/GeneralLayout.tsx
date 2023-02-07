41a40759f0ca8a256a45f140a9fa27c5d6f42add
Völcsey Zoltán <41576384+zvolcsey@users.noreply.github.com>
2023-02-07 12:00:56 +0100

    Fix: fix alignment of TopBar component's elements

--- layouts/GeneralLayout.tsx
+++ layouts/GeneralLayout.tsx
@@ -5,7 +5,7 @@ const GeneralLayout = ({ children }: { children: ReactNode }) => {
   return (
     <section>
       <SideNavbar />
-      <div className="bg-gray-100 dark:bg-[#101623] transition-all ease-in duration-300 mx-auto mt-20 min-h-[calc(100vh-80px)] md:pb-8 lg:ml-20 lg:mt-0 lg:min-h-screen lg:pl-[290px]">
+      <div className="mx-auto mt-20 min-h-[calc(100vh-80px)] bg-gray-100 px-4 transition-all duration-300 ease-in dark:bg-[#101623] md:pb-8 lg:ml-20 lg:mt-0 lg:min-h-screen lg:pl-[290px]">
         {children}
       </div>
     </section>
