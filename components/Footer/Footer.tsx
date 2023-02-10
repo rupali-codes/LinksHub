10415fbb7bc00da12d773c58d7c3230d8af8a13e
aadarsh805 <aadarshthakur80501@gmail.com>
2023-02-10 11:38:01 +0530

    footer backgrund changed to backdrop-blur

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -7,8 +7,8 @@ interface FooterProps {}
 
 export const Footer: FC<FooterProps> = ({}) => {
   return (
-    <div className="w-full flex items-baseline justify-center z-20 gap-1 rounded-lg bg-[#343434] sm:py-3 py-2 px-1">
-      <p className="md:text-lg text-center text-white ">
+    <div className="w-full flex items-baseline justify-center z-20 gap-1 rounded-lg bg-[#ffffff0a] backdrop-blur-md sm:py-3 py-2 px-1">
+      <p className="md:text-lg text-center text-white">
         Open source |{" "}
         <Link
           href="https://github.com/rupali-codes/LinksHub/blob/main/LICENSE"
