1f3755f090e7328ef8f1a87d334f32671c2bc34d
Harsh <hparashar617@gmail.com>
2023-02-17 14:54:08 +0530

    fix of the footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ interface FooterProps {}
 export const Footer: FC<FooterProps> = ({}) => {
   return (
     <div className="w-full flex items-baseline justify-center z-10 gap-1 rounded-lg bg-[#ffffff0a] backdrop-blur-md sm:py-3 py-2 px-1 md:mt-4">
-      <p className="md:text-lg text-center text-gray-200">
+      <p className="md:text-lg text-center text-gray">
         Open source |{" "}
         <Link
           href="https://github.com/rupali-codes/LinksHub/blob/main/LICENSE"
