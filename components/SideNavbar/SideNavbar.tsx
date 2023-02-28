6c12fd33d1333bde7bb6bb99d6453ef7fc0f0f19
Völcsey Zoltán <41576384+zvolcsey@users.noreply.github.com>
2023-02-28 10:49:53 +0100

    feat: Added backdrop

--- components/SideNavbar/SideNavbar.tsx
+++ components/SideNavbar/SideNavbar.tsx
@@ -8,6 +8,7 @@ import Logo from "../logo";
 import classNames from "classnames";
 import { useTheme } from "next-themes";
 import { SideNavbarCategoryList } from "./SideNavbarCategoryList";
+import { Backdrop } from "components/Backdrop/Backdrop";
 
 export const SideNavbar = () => {
   const { toggleNav, sidebar, openNav, closeNav } = useContext(GlobalContext);
@@ -39,49 +40,52 @@ export const SideNavbar = () => {
   const { setSearch, searchResults } = useSidebarSearch();
 
   return (
-    <div
-      className={`lg:w-[290px] fixed top-0 left-0 w-full h-[87px] z-[10] lg:h-full`}
-    >
-      <div className="flex bg-gray-100 p-4 justify-between dark:bg-gray-900 transition-all ease-in duration-300">
-        <Link href={"/"}>
-          <Logo className="text-3xl mb-4" />
-        </Link>
-
-        <button
-          ref={menuBtnRef}
-          className="relative h-[24px] w-[24px] lg:hidden dark:text-gray-300"
-          onClick={toggleNav && toggleNav}
-        >
-          <AiOutlineMenu
-            size={24}
-            className={`transition-all left-0 top-[5%] duration-[.5s] absolute ${
-              sidebar ? "z-[-1] opacity-[0]" : "opacity-[1] z-[1]"
-            }`}
-          />
-          <AiOutlineClose
-            size={24}
-            id="hamburger"
-            className={`absolute left-0 top-[5%] transition-all duration-[1s] ${
-              sidebar
-                ? "opacity-[1] z-[1]"
-                : "opacity-[0] rotate-[180deg] z-[-1]"
-            }`}
-          />
-        </button>
-      </div>
+    <>
+      {sidebar && <Backdrop onClick={closeNav} />}
       <div
-        ref={menuRef}
-        className={classNames(
-          `lg:translate-x-0 lg:w-full w-[75%] bg-base-200 transition-all whitespace-nowrap ease-in duration-300 overflow-x-hidden h-screen z-[10] dark:bg-gray-900 dark:text-gray-300`,
-          sidebar ? "translate-x-[0%] " : "translate-x-[-100%]",
-          theme === "light" ? "scrollColorLight" : "scrollColorDark"
-        )}
+        className={`lg:w-[290px] fixed top-0 left-0 w-full h-[87px] z-[100] lg:h-full`}
       >
-        <div className="sticky top-0 left-0 right-0 p-4 w-full z-10 bg-base-200 dark:bg-gray-900 transiton-all duration-300 ease-in">
-          <Searchbar setSearch={setSearch} />
+        <div className="flex bg-gray-100 p-4 justify-between dark:bg-gray-900 transition-all ease-in duration-300">
+          <Link href={"/"}>
+            <Logo className="text-3xl mb-4" />
+          </Link>
+
+          <button
+            ref={menuBtnRef}
+            className="relative h-[24px] w-[24px] lg:hidden dark:text-gray-300"
+            onClick={toggleNav && toggleNav}
+          >
+            <AiOutlineMenu
+              size={24}
+              className={`transition-all left-0 top-[5%] duration-[.5s] absolute ${
+                sidebar ? "z-[-1] opacity-[0]" : "opacity-[1] z-[1]"
+              }`}
+            />
+            <AiOutlineClose
+              size={24}
+              id="hamburger"
+              className={`absolute left-0 top-[5%] transition-all duration-[1s] ${
+                sidebar
+                  ? "opacity-[1] z-[1]"
+                  : "opacity-[0] rotate-[180deg] z-[-1]"
+              }`}
+            />
+          </button>
         </div>
-        <SideNavbarCategoryList items={searchResults} openByDefault={'frontend'} />
+        <div
+          ref={menuRef}
+          className={classNames(
+            `lg:translate-x-0 lg:w-full w-[75%] bg-base-200 transition-all whitespace-nowrap ease-in duration-300 overflow-x-hidden h-screen z-[10] dark:bg-gray-900 dark:text-gray-300`,
+            sidebar ? "translate-x-[0%] " : "translate-x-[-100%]",
+            theme === "light" ? "scrollColorLight" : "scrollColorDark"
+          )}
+        >
+          <div className="sticky top-0 left-0 right-0 p-4 w-full z-10 bg-base-200 dark:bg-gray-900 transiton-all duration-300 ease-in">
+            <Searchbar setSearch={setSearch} />
+          </div>
+          <SideNavbarCategoryList items={searchResults} openByDefault={'frontend'} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
