13af61a12b216cd5b0dea1987be245f8c629166a
ujjawaltyagii <ujjawal2002tyagi@gmail.com>
2023-03-04 22:06:38 +0530

    overflow bug fixed

--- components/cards/index.tsx
+++ components/cards/index.tsx
@@ -13,7 +13,7 @@ const Cards = () => {
   };
   return (
     <div
-      className={`flex flex-wrap md:flex-row w-full md:justify-start gap-4 content-start lg:min-h-[calc(90vh-80px)] min-h-[calc(90vh-150px)] mb-2 ${
+      className={`flex flex-wrap md:flex-row w-full md:justify-start gap-4 content-start lg:min-h-[calc(90vh-99px)] min-h-[calc(90vh-150px)] mb-[0.29rem] md:-mb-[1.7rem] ${
         filterDB.length < 3 && "lg:justify-start"
       }`}
     >
