37de498003990fea2c93d86db361a4e12d97ab69
Zoltán Völcsey <41576384+zvolcsey@users.noreply.github.com>
2023-03-07 17:08:40 +0100

    feat: Improved the site structure (#303)
    * feat: Improved the site structure
    
    * fix: Changed the bg to the same color as the header
    
    * fix: Fixed the header
    
    * fix: Added animations to sidebar and popup

--- components/Backdrop/Backdrop.tsx
+++ components/Backdrop/Backdrop.tsx
@@ -1,12 +1,12 @@
 import { FC } from "react";
 import { createPortal } from "react-dom";
 
-export const Backdrop: FC<{ onClick: (() => void) | undefined}> = (props) => {
-  const { onClick } = props;
+export const Backdrop: FC<{ onClick: (() => void) | undefined, className?: string | undefined }> = (props) => {
+  const { onClick, className } = props;
 
   return createPortal(
     <div 
-      className={`fixed inset-0 z-50 h-full w-full cursor-pointer bg-black/80 lg:hidden`}
+      className={`fixed inset-0 z-50 h-full w-full cursor-pointer bg-black/80 ${className}`}
       onClick={onClick}
     ></div>,
     document.getElementById('backdrop-root')!
