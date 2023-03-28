33a5ba5492fc7f2de711bc3ebba6410e278ffcb0
Manikandan R S <rsmanikandan.dev@gmail.com>
2023-03-28 18:01:17 +0530

    fix: changed from react-uuid to uuid (#381)
    * changed from react-uuid to uuid
    
    * added TS uuid

--- database/data.ts
+++ database/data.ts
@@ -1,69 +1,70 @@
-import { IDBData, IData, ISidebar } from "../types";
-import * as DB from "database";
-import uuid from "react-uuid";
+import { IDBData, IData, ISidebar } from '../types'
+import * as DB from 'database'
+import { v4 as uuidv4 } from 'uuid'
 
-export const database:IData[][] = Object.values(DB).map((item: IDBData[])=>item.map(subcat=>({...subcat,id:uuid()})));
+export const database: IData[][] = Object.values(DB).map((item: IDBData[]) =>
+  item.map((subcat) => ({ ...subcat, id: uuidv4() }))
+)
 
 export const sidebarData: ISidebar[] = [
   {
-    category: "frontend",
+    category: 'frontend',
     subcategory: [
-      { name: "images", url: "/images" },
-      { name: "fonts", url: "/fonts" },
-      { name: "colors", url: "/colors" },
-      { name: "illustrations", url: "/illustrations" },
-      { name: "animations", url: "/animations" },
-      { name: "icons", url: "/icons" },
-      { name: "design inspiration", url: "/design-inspiration" },
-      { name: "themes & templates", url: "/themes-templates" },
-      { name: "UI Generators", url: "/ui-generator" },
-      { name: "online code editors", url: "/online-code-editors" },
+      { name: 'images', url: '/images' },
+      { name: 'fonts', url: '/fonts' },
+      { name: 'colors', url: '/colors' },
+      { name: 'illustrations', url: '/illustrations' },
+      { name: 'animations', url: '/animations' },
+      { name: 'icons', url: '/icons' },
+      { name: 'design inspiration', url: '/design-inspiration' },
+      { name: 'themes & templates', url: '/themes-templates' },
+      { name: 'UI Generators', url: '/ui-generator' },
+      { name: 'online code editors', url: '/online-code-editors' },
     ],
   },
   {
-    category: "backend",
+    category: 'backend',
     subcategory: [
-      { name: "architecture", url: "/architecture" },
-      { name: "validation", url: "/validation" },
-      { name: "security", url: "/security" },
-      { name: "authentication", url: "/authentication" },
-      { name: "caching", url: "/caching" },
-      { name: "testing", url: "/testing" },
+      { name: 'architecture', url: '/architecture' },
+      { name: 'validation', url: '/validation' },
+      { name: 'security', url: '/security' },
+      { name: 'authentication', url: '/authentication' },
+      { name: 'caching', url: '/caching' },
+      { name: 'testing', url: '/testing' },
     ],
   },
   {
-    category: "youtube",
+    category: 'youtube',
     subcategory: [
-      { name: "web development", url: "/web-development" },
-      { name: "css", url: "/css" },
-      { name: "machine learning", url: "/machine-learning" },
-      { name: "data structures", url: "/dsa" },
-      { name: "Android", url: "/android" },
-      { name : "Web3 & Metaverse", url : "/web3-metaverse"},
-      { name : "Testing", url : "/testing"},
-      
+      { name: 'web development', url: '/web-development' },
+      { name: 'css', url: '/css' },
+      { name: 'machine learning', url: '/machine-learning' },
+      { name: 'data structures', url: '/dsa' },
+      { name: 'Android', url: '/android' },
+      { name: 'Web3 & Metaverse', url: '/web3-metaverse' },
+      { name: 'Testing', url: '/testing' },
     ],
   },
   {
-    category: "resources",
+    category: 'resources',
     subcategory: [
-      { name: "blogs", url: "/blogs" },
-      { name: "hosting", url: "/hosting" },
+      { name: 'blogs', url: '/blogs' },
+      { name: 'hosting', url: '/hosting' },
     ],
   },
   {
-    category: "languages",
+    category: 'languages',
     subcategory: [
-      { name: "JavaScript", url: "/javascript"},
-      { name: "Python", url: "/python"},
-      { name: "Go", url: "/golang"},
+      { name: 'JavaScript', url: '/javascript' },
+      { name: 'Python', url: '/python' },
+      { name: 'Go', url: '/golang' },
     ],
   },
   {
-    category: "Other",
+    category: 'Other',
     subcategory: [
-      { name: "Github", url: "/github" },
-      { name: "Dev Tools", url: "/devtools"},
+      { name: 'Github', url: '/github' },
+      { name: 'Dev Tools', url: '/devtools' },
     ],
-  }
-];
+  },
+]
