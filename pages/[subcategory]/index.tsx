6a1c6499af7d5f87b139f1f191f99f3bc5e27eb1
Vikas Kumar <architectvikas272003@gmail.com>
2023-04-22 07:45:24 +0000

    add theme color meta tag to all pages

--- pages/[subcategory]/index.tsx
+++ pages/[subcategory]/index.tsx
@@ -25,6 +25,7 @@ const SubCategory = () => {
     <>
       <Head>
         <title>{title}</title>
+        <meta name="theme-color" content="#202c46"/>
       </Head>
       <TopBar className="shadow-black-500/50 fixed top-[76px] z-30 flex w-full -translate-x-4 items-center bg-gray-100 px-4 pt-6 pb-4 shadow-xl dark:bg-gray-900 md:hidden" />
       <div className="min-h-[calc(100%-68px)] w-full pt-[85px] pb-4 md:min-h-[calc(100%-76px)] md:px-10 md:pt-10">
