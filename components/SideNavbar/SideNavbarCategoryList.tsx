dddf0c2ea2c3185f8d1d62a7a4b377ddac70a71f
aashay shah <aashayshah28@gmail.com>
2023-04-26 12:04:02 +0530

    [Feature] Add default text when search is empty #480

--- components/SideNavbar/SideNavbarCategoryList.tsx
+++ components/SideNavbar/SideNavbarCategoryList.tsx
@@ -1,17 +1,28 @@
-import { FC } from "react";
-import type { ISidebar } from "../../types";
-import { SideNavbarCategory } from "./SideNavbarCategory";
+import { FC } from 'react'
+import type { ISidebar } from '../../types'
+import { SideNavbarCategory } from './SideNavbarCategory'
 
-export const SideNavbarCategoryList: FC<{items: ISidebar[], openByDefault: string}> = (props) => {
-  const { items, openByDefault } = props;
-
+export const SideNavbarCategoryList: FC<{
+  items: ISidebar[]
+  openByDefault: string
+}> = (props) => {
+  const { items, openByDefault } = props
+  console.log('items', items)
   return (
     <ul className="mt-2 flex flex-col justify-center px-4 pb-24">
-      {items.map((item, index) => {
-        return (
-          <SideNavbarCategory key={index} item={item} openByDefault={openByDefault} />
-        );
-      })}
+      {items.length !== 0 ? (
+        items.map((item, index) => {
+          return (
+            <SideNavbarCategory
+              key={index}
+              item={item}
+              openByDefault={openByDefault}
+            />
+          )
+        })
+      ) : (
+        <div className="text-slate-500 py-2">No Links Found</div>
+      )}
     </ul>
-  );
-};
+  )
+}
