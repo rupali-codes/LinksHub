021f161daec2814b5615f95bf3722a2178d5249b
Aashay Shah <108337259+aashay28@users.noreply.github.com>
2023-04-27 00:51:58 +0530

    feat: added default text when search is empty (#483)
    * [Feature] Add default text when search is empty #480
    
    * Update components/SideNavbar/SideNavbarCategoryList.tsx
    
    Co-authored-by: Rupali Haldiya <78981177+rupali-codes@users.noreply.github.com>
    
    * Update components/SideNavbar/SideNavbarCategoryList.tsx
    
    Co-authored-by: Imbeshat Aslam <48837703+imbeshat@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Rupali Haldiya <78981177+rupali-codes@users.noreply.github.com>
    Co-authored-by: Imbeshat Aslam <48837703+imbeshat@users.noreply.github.com>

--- components/SideNavbar/SideNavbarCategoryList.tsx
+++ components/SideNavbar/SideNavbarCategoryList.tsx
@@ -1,17 +1,27 @@
-import { FC } from "react";
-import type { ISidebar } from "../../types";
-import { SideNavbarCategory } from "./SideNavbarCategory";
+import { FC } from 'react'
+import type { ISidebar } from '../../types'
+import { SideNavbarCategory } from './SideNavbarCategory'
 
-export const SideNavbarCategoryList: FC<{items: ISidebar[], openByDefault: string}> = (props) => {
-  const { items, openByDefault } = props;
-
+export const SideNavbarCategoryList: FC<{
+  items: ISidebar[]
+  openByDefault: string
+}> = (props) => {
+  const { items, openByDefault } = props
   return (
     <ul className="mt-2 flex flex-col justify-center px-4 pb-24">
-      {items.map((item, index) => {
-        return (
-          <SideNavbarCategory key={index} item={item} openByDefault={openByDefault} />
-        );
-      })}
+      {items.length !== 0 ? (
+        items.map((item, index) => {
+          return (
+            <SideNavbarCategory
+              key={index}
+              item={item}
+              openByDefault={openByDefault}
+            />
+          )
+        })
+      ) : (
+        <div className="dark:text-gray-200 text-gray-500 text-lg text-center py-2">No Links Found</div>
+      )}
     </ul>
-  );
-};
+  )
+}
