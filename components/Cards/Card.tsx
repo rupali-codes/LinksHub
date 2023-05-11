9b24142ce79c77d242849ca5db8d7cf5b165b550
Josh Goldberg <git@joshuakgoldberg.com>
2023-05-11 11:45:38 -0400

    fix: accessibility issues

--- components/Cards/Card.tsx
+++ components/Cards/Card.tsx
@@ -8,11 +8,11 @@ const Card: FC<{ data: IData }> = (props) => {
   const { name, description, url } = data
 
   return (
-    <article className="z-10 h-full w-full rounded-3xl border border-dashed border-violet-500 bg-gray-100 shadow-lg dark:bg-gray-900 dark:text-gray-300 dark:shadow-sm">
+    <article className="z-10 h-full w-full rounded-3xl border border-dashed border-violet-500 dark:border-violet-400 bg-gray-100 shadow-lg dark:bg-gray-900 dark:text-gray-300 dark:shadow-sm">
       <div className="card-body">
         <header className="flex justify-between items-center">
           <h2
-            className="cursor-default truncate ... text-xl text-violet-500"
+            className="cursor-default truncate ... text-xl text-violet-600 dark:text-violet-400"
             title={name}
           >
             {name}
@@ -27,7 +27,7 @@ const Card: FC<{ data: IData }> = (props) => {
             target="_blank"
             rel="noreferrer"
             className={
-              'mt-2 flex w-full items-center justify-center gap-2 rounded-2xl border border-dashed border-transparent bg-violet-600 px-6 py-2 text-center text-white duration-100 hover:border-violet-400 hover:bg-transparent hover:text-violet-500'
+              'mt-2 flex w-full items-center justify-center gap-2 rounded-2xl border border-dashed border-transparent bg-violet-600 px-6 py-2 text-center text-white duration-100 hover:border-violet-400 hover:bg-transparent hover:text-violet-500 dark:hover:text-violet-400'
             }
           >
             Visit site
