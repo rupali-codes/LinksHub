bbbc4fc190f859d1cb241ff218759235c7a2de24
Emma Dawson <emma.l.dawson@gmail.com>
2023-05-19 21:34:07 +0200

    Add SkipLink component

---
+++ components/SkipLink/SkipLink.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+
+export const SkipLink: FC = () => (
+  <div className="z-40 flex absolute left-1/2 transform -translate-x-1/2">
+    <a
+      href="#main"
+      className="-translate-y-20   focus:transform-none mt-2 flex items-center justify-center gap-2 rounded-2xl border border-dashed border-transparent bg-violet-600 px-6 py-2 text-center text-white"
+    >
+      Skip to main content
+    </a>
+  </div>
+)
