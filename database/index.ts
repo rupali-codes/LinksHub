9903098873fd097ab6b34db2354149e15474d1f7
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-06-03 12:05:59 +0530

    feat: add videos subcategory in frontend

--- database/index.ts
+++ database/index.ts
@@ -11,6 +11,7 @@ export { default as onlineCodeEditors } from './frontend/online-code-editors.jso
 export { default as themesTemplates } from './frontend/themes-templates.json'
 export { default as uiGenerators } from './frontend/ui-generators.json'
 export { default as react } from './frontend/react.json'
+export { default as videos } from './frontend/videos.json'
 // backend
 export { default as authentication } from './backend/authentication.json'
 export { default as architecture } from './backend/architecture.json'
