fa38bbfc8d2ba521437e44a113de84a01c8006d0
Christine Belzie <105683440+CBID2@users.noreply.github.com>
2023-06-03 14:13:58 -0400

    Update Header.tsx

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -9,7 +9,16 @@ import { SocialMediaIconsList } from "components/SocialMedia/SocialMediaIconsLis
 
 export const Header:FC<{}> = () => {
   const { toggleNav } = useContext(GlobalContext);
-
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = 'https://buttons.github.io/buttons.js'
+    script.async = true
+    script.defer = true
+    document.head.appendChild(script)
+    return () => {
+      document.head.removeChild(script)
+    }
+  }, [])
   return (
     <header className="fixed top-0 left-0 z-30 row-start-1 row-end-2 flex h-[76px] w-screen items-center justify-between">
       <div className="bg-base-200 h-full w-fit flex-none px-6 py-4 dark:bg-gray-900 lg:w-[290px]">
@@ -18,6 +27,16 @@ export const Header:FC<{}> = () => {
         </Link>
       </div>
       <div className="bg-base-200 relative h-full grow px-8 dark:bg-gray-900 lg:bg-gray-100 lg:dark:bg-[#101623]">
+        <Link
+          className="github-button"
+          href="https://github.com/rupali-codes/LinksHub"
+          data-icon="octicon-repo-forked"
+          data-size="large"
+          data-show-count="true"
+          aria-label="Fork rupali-codes/LinksHub on GitHub"
+        >
+          Fork
+        </Link>
         <TopBar className="absolute left-8 hidden h-full md:flex" />
         <div className="absolute right-8 flex h-full gap-4">
           <SocialMediaIconsList className={"hidden lg:flex"} />
