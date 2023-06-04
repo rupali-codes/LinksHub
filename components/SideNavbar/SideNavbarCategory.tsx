0e18aa08a12730f29e083ff5633c1112ffd193e4
Ujjawal Tyagi <115401171+ujjawaltyagii@users.noreply.github.com>
2023-06-04 18:53:33 +0530

    Update SideNavbarCategory.tsx

--- components/SideNavbar/SideNavbarCategory.tsx
+++ components/SideNavbar/SideNavbarCategory.tsx
@@ -1,43 +1,51 @@
-import { FC } from 'react'
-import { FaAngleDown } from 'react-icons/fa'
-import { SideNavbarElement } from './SideNavbarElement'
-import type { ISidebar, Category } from '../../types'
+import { FC, useState } from 'react';
+import { FaAngleDown } from 'react-icons/fa';
+import { SideNavbarElement } from './SideNavbarElement';
+import type { ISidebar, Category } from '../../types';
 
 export const SideNavbarCategory: FC<{
-  item: ISidebar
-  handleToggle: (category: Category, isOpen: boolean) => void
-  isOpen: boolean
+  item: ISidebar;
+  handleToggle: (category: Category, isOpen: boolean) => void;
+  isOpen: boolean;
 }> = (props) => {
-  const { item, isOpen } = props
+  const { item, isOpen } = props;
+  const [isExpanded, setExpanded] = useState(false);
 
   const handleToggle = () => {
-    props.handleToggle(item.category, isOpen)
-  }
+    props.handleToggle(item.category, isOpen);
+    setExpanded(!isExpanded);
+  };
 
-  const subcategoryList = (
-    <ul className="relative ml-1 border-l-2 border-slate-300 dark:border-slate-700 -pl-0.5">
-      {item.subcategory
-        .sort((a, b) =>
-          a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1
-        )
-        .map((list, i) => {
-          return (
-            <li className="-ml-0.5" key={i}>
-              <SideNavbarElement {...list} />
-            </li>
-          )
-        })}
-    </ul>
-  )
+  const subcategoryList = item.subcategory
+    .sort((a, b) => a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1)
+    .map((list, i) => (
+      <li className="-ml-0.5" key={i}>
+        <SideNavbarElement {...list} />
+      </li>
+    ));
 
   return (
-    <li className="relative w-full transition-all ease-in-out overflow-hidden text-violet-600 dark:text-violet-400 dark:bg-opacity-5 hover:text-violet-500 dark:hover:text-violet-300 rounded-md focus-visible:outline-none focus-visible:ring focus-visible:ring-violet-400">
-      <input type="checkbox" className="peer absolute top-0 inset-x-0 w-full h-12 opacity-0 z-10 cursor-pointer"/>
-      <h1 className="font-bold uppercase h-12 flex items-center">{item.category}</h1>
-      <FaAngleDown className="absolute top-3.5 right-0 transition-all duration-500 ease-in-out peer-checked:rotate-180" />
-      <div className="transition-all duration-500 ease-in-out overflow-auto max-h-0 peer-checked:max-h-screen">
-        {subcategoryList}
+    <li className="relative w-full transition-all ease-in-out text-violet-600 dark:text-violet-400 dark:bg-opacity-5 hover:text-violet-500 dark:hover:text-violet-300 rounded-md focus-visible:outline-none focus-visible:ring focus-visible:ring-violet-400">
+      <button
+        className="flex w-full cursor-pointer justify-between py-2"
+        onClick={handleToggle}
+      >
+        <h1 className="font-bold uppercase">{item.category}</h1>
+        <FaAngleDown
+          className={`${
+            isOpen && 'rotate-180'
+          } self-center transition duration-300 ease-in-out`}
+        />
+      </button>
+      <div
+        className={`overflow-hidden transition-all duration-500 ease-in-out max-h-0 ${
+          isExpanded ? 'max-h-screen' : ''
+        }`}
+      >
+        <ul className="relative ml-1 border-l-2 border-slate-300 dark:border-slate-700 -pl-0.5">
+          {subcategoryList}
+        </ul>
       </div>
     </li>
-  )
-}
+  );
+};
