67befa94fe0ac4e622d5e6aa44b556cd6b455fb1
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-06-05 12:46:00 +0530

    feat: add github fork button

--- components/SocialMedia/SocialMediaIconsList.tsx
+++ components/SocialMedia/SocialMediaIconsList.tsx
@@ -1,20 +1,44 @@
 import { FC } from "react";
 import { IconContext } from "react-icons";
-import {FaDiscord, FaGithub, FaTwitter} from "react-icons/fa";
+import { FaDiscord, FaGithub, FaTwitter } from "react-icons/fa";
+import Link from "next/link";
+import { useEffect } from "react";
 
-export const SocialMediaIconsList:FC<{className?: string}> = (props) => {
+export const SocialMediaIconsList: FC<{ className?: string }> = (props) => {
   const { className } = props;
 
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = 'https://buttons.github.io/buttons.js'
+    script.async = true
+    script.defer = true
+    document.head.appendChild(script)
+    return () => {
+      document.head.removeChild(script)
+    }
+  }, [])
   return (
     <ul className={`flex items-center gap-6 ${className}`}>
+      <li className="mt-2 mr-2 hidden md:block">
+        <Link
+          className="github-button"
+          href="https://github.com/rupali-codes/LinksHub"
+          data-icon="octicon-repo-forked"
+          data-size="large"
+          data-show-count="true"
+          aria-label="Fork rupali-codes/LinksHub on GitHub"
+        >
+          Fork
+        </Link>
+      </li>
       <li>
         <a
-        title="Link to Discord server (External Link)"
-        className="dark:text-gray-300"
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://discord.com/invite/NvK67YnJX5"
-        aria-label="Visit us on Discord"
+          title="Link to Discord server (External Link)"
+          className="dark:text-gray-300"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://discord.com/invite/NvK67YnJX5"
+          aria-label="Visit us on Discord"
         >
           <IconContext.Provider
             value={{ className: "shared-class", size: "24" }}
@@ -25,17 +49,17 @@ export const SocialMediaIconsList:FC<{className?: string}> = (props) => {
       </li>
       <li>
         <a
-        title="Link to Github project (External Link)"
-        className="dark:text-gray-300"
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://github.com/rupali-codes/LinksHub"
-        aria-label="Visit us on Github"
+          title="Link to Github project (External Link)"
+          className="dark:text-gray-300"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/rupali-codes/LinksHub"
+          aria-label="Visit us on Github"
         >
           <IconContext.Provider
             value={{ className: "shared-class", size: "24" }}
           >
-              <FaGithub className="hover:text-violet-500 transition duration-300 ease-in-out" />
+            <FaGithub className="hover:text-violet-500 transition duration-300 ease-in-out" />
           </IconContext.Provider>
         </a>
       </li>
@@ -48,11 +72,11 @@ export const SocialMediaIconsList:FC<{className?: string}> = (props) => {
           href="https://twitter.com/linkshubdotdev"
           aria-label="Visit us on Twitter"
         >
-        <IconContext.Provider
-          value={{ className: "shared-class", size: "24" }}
-        >
-          <FaTwitter className="hover:text-violet-500 transition duration-300 ease-in-out" />
-        </IconContext.Provider>
+          <IconContext.Provider
+            value={{ className: "shared-class", size: "24" }}
+          >
+            <FaTwitter className="hover:text-violet-500 transition duration-300 ease-in-out" />
+          </IconContext.Provider>
         </a>
       </li>
     </ul>
