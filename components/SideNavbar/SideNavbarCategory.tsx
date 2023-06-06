75b6f0129d11bfb8933fa7a572fc0eb64a77aa88
Ali Reza <webdeve1083@gmail.com>
2023-06-06 09:09:20 +0800

    Added height animation for sub lists when opening and closing

--- components/SideNavbar/SideNavbarCategory.tsx
+++ components/SideNavbar/SideNavbarCategory.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 import { FaAngleDown } from 'react-icons/fa'
 import { SideNavbarElement } from './SideNavbarElement'
 import type { ISidebar, Category } from '../../types'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export const SideNavbarCategory: FC<{
   item: ISidebar
@@ -18,26 +19,33 @@ export const SideNavbarCategory: FC<{
 
   if (props.isOpen) {
     subcategoryList = (
-      <ul className="relative ml-1 border-l-2 border-slate-300 dark:border-slate-700 -pl-0.5">
-        {item.subcategory
-          .sort((a, b) =>
-            a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1
-          )
-          .map((list, i) => {
-            return (
-              <li className="-ml-0.5" key={i}>
-                <SideNavbarElement {...list} />
-              </li>
+      <motion.div
+        initial={{ height: 0 }}
+        animate={{ height: 'auto' }}
+        exit={{ height: 0 }}
+        className="overflow-hidden"
+      >
+        <ul className="relative ml-1 border-l-2 border-slate-300 dark:border-slate-700 -pl-0.5">
+          {item.subcategory
+            .sort((a, b) =>
+              a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1
             )
-          })}
-      </ul>
+            .map((list, i) => {
+              return (
+                <li className="-ml-0.5" key={i}>
+                  <SideNavbarElement {...list} />
+                </li>
+              )
+            })}
+        </ul>
+      </motion.div>
     )
   }
 
   return (
     <li className="mb-2">
       <button
-        className="flex w-full cursor-pointer justify-between py-2 text-violet-600 dark:text-violet-400 dark:bg-opacity-5 hover:text-violet-500 dark:hover:text-violet-300 rounded-md focus-visible:outline-none focus-visible:ring focus-visible:ring-violet-400"
+        className="flex justify-between w-full py-2 rounded-md cursor-pointer text-violet-600 dark:text-violet-400 dark:bg-opacity-5 hover:text-violet-500 dark:hover:text-violet-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-violet-400"
         onClick={handleToggle}
       >
         <h2 className="font-bold uppercase">{item.category}</h2>
@@ -47,7 +55,7 @@ export const SideNavbarCategory: FC<{
           } self-center transition duration-300 ease-in-out`}
         />
       </button>
-      {subcategoryList}
+      <AnimatePresence initial={false}>{subcategoryList}</AnimatePresence>
     </li>
   )
 }
