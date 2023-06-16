749b21f69fd0dfbe5ace20f34c05db46e91de2e3
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-06-16 22:40:09 +0530

    feat: write code for modal to show respective desc

--- components/TopBar/TopBar.tsx
+++ components/TopBar/TopBar.tsx
@@ -1,15 +1,34 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { useRouter } from 'next/router'
 import { FaSlackHash, FaInfoCircle } from 'react-icons/fa'
 import PopupDesc from 'components/popup/popupCategoryDesc'
+import { ICategoryData } from 'types'
+import categoryDescriptions from './CategoryDescriptions'
 
 export const TopBar: FC<{ className?: string | undefined }> = (props) => {
   const { className } = props
+  const [currentCategory, setCurrentCategory] = useState<ICategoryData | null>(
+    null
+  )
   const router = useRouter()
   const category = router.asPath.replace('/', '')
+  const categoryName = category.split('-').join(' ')
 
   if (router.pathname.length === 1) {
     return null
+  }
+
+  const handleCardClick = () => {
+    const description = categoryDescriptions[categoryName] || ''
+    const categoryInfo = {
+      name: categoryName,
+      description,
+    }
+    setCurrentCategory(categoryInfo)
+  }
+
+  const removeCurrentCard = () => {
+    setCurrentCategory(null)
   }
 
   return (
@@ -19,8 +38,14 @@ export const TopBar: FC<{ className?: string | undefined }> = (props) => {
       <FaSlackHash className="mr-2 text-gray-600 dark:text-gray-300" />
       <span className="flex font-semmibold uppercase text-gray-900 dark:text-gray-100">
         {category.split('-').join(' ')}
-        <FaInfoCircle className="ml-4 mt-2 text-sm" />
-        {/* <PopupDesc currentCard={category.split('-').join(' ')} /> */}
+        <FaInfoCircle
+          className="ml-4 mt-2 text-sm cursor-pointer"
+          onClick={handleCardClick}
+        />
+        <PopupDesc
+          currentCategory={currentCategory}
+          onClose={removeCurrentCard}
+        />
       </span>
     </div>
   )
