77f52ba6496b04011a9135573bd88137ad40224a
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-06-16 22:39:37 +0530

    feat: add subcategories data

---
+++ components/TopBar/CategoryDescriptions.ts
@@ -0,0 +1,72 @@
+type CategoryDescriptions = {
+  [key: string]: string
+}
+
+const categoryDescriptions: CategoryDescriptions = {
+  //frontend
+  accessibility: 'Accessibility category description',
+  animations: 'Animations category description',
+  colors: 'Colors category description',
+  designInspirations: 'Design Inspiration category description',
+  fonts: 'Fonts category description',
+  icons: 'Icons category description',
+  illustrations: 'Illustrations category description',
+  images: 'Images category description',
+  onlineCodeEditors: 'Online Code Editors category description',
+  react: 'React category description',
+  themesTemplates: 'Themes and Templates category description',
+  uiGenerators: 'UI Generators category description',
+  videos: 'Videos Category description',
+  //backend
+  architecture: 'Architecture category description',
+  authentication: 'Authentication category description',
+  caching: 'Caching category description',
+  database: 'Database category description',
+  security: 'Security category description',
+  systemDesign: 'System Design category description',
+  testing: 'Testing category description',
+  validation: 'Validation category description',
+  // languages
+  CProgramming: 'CProgramming category description',
+  csharp: 'csharp category description',
+  golang: 'Golang category description',
+  javascript: 'JavaScript category description',
+  python: 'Python category description',
+  ruby: 'ruby category description',
+  typescript: 'typescript category description',
+  // ml & ai
+  dataScience: 'Data Science category description',
+  deepLearning: 'Deep Learning category description',
+  ml: 'ML category description',
+  // Open-source
+  openSourceBlogs: 'Open Source Blogs category description',
+  openSourceProjects: 'Open Source Projects category description',
+  openSourceTools: 'Open Source Tools category description',
+  // resources
+  blogs: 'Blogs category description',
+  ebook: 'E-Book category description',
+  hosting: 'Hosting category description',
+  officialdocs: 'Official Docs category description',
+  project: 'Project category description',
+  // youtube
+  android: 'Android category description',
+  competitiveProgramming: 'Competitive Programming category description',
+  computerScience: 'Computer Science category description',
+  css: 'CSS category description',
+  dataStructures: 'Data Structures category description',
+  fintech: 'Fintech category description',
+  gameDevelopment: 'Game Development category description',
+  machineLearning: 'Machine Learning category description',
+  tensorflow: 'TensorFlow category description',
+  youtubTesting: 'Youtube Testing category description',
+  webDevelopment: 'Web Development category description',
+  web3metaverse: 'Web3 Metaverse category description',
+  // other
+  communities: 'Communities category description',
+  devtools: 'DevTools category description',
+  github: 'GitHub category description',
+  otherResources: 'Other Resources category description',
+  podcasts: 'Podcasts category description',
+}
+
+export default categoryDescriptions
