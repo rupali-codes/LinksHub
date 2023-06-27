194c939981e964644fa8b54eeb7976c2e5210fb7
Swapnil Singh <swapnilskumars@gmail.com>
2023-06-28 03:07:10 +0530

    Added css filters to frontend/ui-generators

--- database/data.ts
+++ database/data.ts
@@ -48,8 +48,8 @@ export const sidebarData: ISidebar[] = [
       { name: 'Ruby', url: '/ruby' },
       { name: 'C Programming', url: '/c-programming' },
       { name: 'C++ Programming', url: '/cpp-programming' },
-      { name:  'Java', url: '/java'},
-      { name:  'Kotlin', url: '/kotlin'}
+      { name: 'Java', url: '/java' },
+      { name: 'Kotlin', url: '/kotlin' },
     ],
   },
   {
