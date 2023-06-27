f456dec01de6ff32996d38b0e36151cf837e53de
Siddartha Dayala <siddarthdayala24@gmail.com>
2023-06-27 14:41:06 +0530

    added pagination

--- components/Cards/CardsList.tsx
+++ components/Cards/CardsList.tsx
@@ -3,10 +3,19 @@ import { BackToTopButton } from '../BackToTop/BackToTopButton'
 import Popup from 'components/popup'
 import CardsListItem from './CardsListItem'
 import type { IData } from 'types'
+import { ConfigProvider, Pagination } from 'antd'
 
 const CardsList: FC<{ cards: IData[] }> = (props) => {
   const { cards } = props
   const [currentCard, setCurrentCard] = useState<IData | null>(null)
+
+  const [page, setPage] = useState(1)
+  const pageSize = 6
+
+  let indexOfFirstPage, indexOfLastPage, currentCards
+  indexOfLastPage = page * pageSize
+  indexOfFirstPage = indexOfLastPage - pageSize
+  currentCards = cards.slice(indexOfFirstPage, indexOfLastPage)
 
   const getCardId = (item: IData | null) => {
     setCurrentCard(item)
@@ -24,8 +33,10 @@ const CardsList: FC<{ cards: IData[] }> = (props) => {
 
   return (
     <>
-      <ul className={`grid grid-cols-1 md:grid-cols-3 gap-4 justify-items-stretch`}>
-        {cards.map((data: IData) => (
+      <ul
+        className={`grid grid-cols-1 md:grid-cols-3 gap-4 justify-items-stretch`}
+      >
+        {currentCards.map((data: IData) => (
           <CardsListItem
             key={data.id}
             data={data}
@@ -33,6 +44,28 @@ const CardsList: FC<{ cards: IData[] }> = (props) => {
           />
         ))}
       </ul>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorText: 'white',
+            colorTextDisabled: 'white',
+            //colorBgContainer: '#7c3aed'
+          },
+        }}
+      > 
+        <Pagination
+          className="bg-violet-600 table mx-auto mt-5 p-2 rounded-full "
+          size='small'
+          responsive
+          total={cards.length}
+          current={page}
+          pageSize={pageSize}
+          onChange={(page) => {
+            setPage(page)
+          }}
+        />
+      </ConfigProvider>
+
       <BackToTopButton />
       <Popup currentCard={currentCard} onClose={removeCurrentCard} />
     </>
