bb7aa7b41ebc0e1c881340c2a598a24449c89fca
Christine Belzie <105683440+CBID2@users.noreply.github.com>
2023-07-04 13:21:31 -0400

    feat: adding gh action for spelling errors (#1174)

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,23 +1,47 @@
-import { FC, useContext } from 'react'
-import Link from 'next/link'
-import { AiOutlineMenu } from 'react-icons/ai'
-import Logo from 'components/logo'
-import { GlobalContext } from 'context/GlobalContext'
-import { ThemeToggler } from '../ThemeToggler/themeToggler'
-import { TopBar } from '../TopBar/TopBar'
-import { SocialMediaIconsList } from 'components/SocialMedia/SocialMediaIconsList'
+import { FC, useContext, useEffect } from "react";
+import Link from "next/link";
+import { AiOutlineMenu } from "react-icons/ai";
+import Logo from "components/logo";
+import { GlobalContext } from "context/GlobalContext";
+import { ThemeToggler } from "../ThemeToggler/themeToggler";
+import { TopBar } from "../TopBar/TopBar";
+import { SocialMediaIconsList } from "components/SocialMedia/SocialMediaIconsList";
 
 export const Header: FC<{}> = () => {
-  const { toggleNav } = useContext(GlobalContext)
+  const { toggleNav } = useContext(GlobalContext);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://buttons.github.io/buttons.js';
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
 
   return (
-    <header className="fixed top-0 left-0 z-30 row-start-1 row-end-2 flex h-[76px] w-screen items-center justify-between">
-      <div className="bg-base-200 h-full w-fit flex-none px-6 py-4 dark:bg-gray-900 lg:w-[290px]">
+    <div className="flex items-center justify-between p-4">
+      <div className="flex items-center gap-2">
+        <AiOutlineMenu className="text-2xl cursor-pointer" onClick={toggleNav} />
         <Link href="/">
-          <Logo className="text-3xl" />
+          <a className="font-bold text-xl">
+            <Logo />
+          </a>
         </Link>
       </div>
       <div className="bg-base-200 relative h-full grow px-8 dark:bg-gray-900 lg:bg-gray-100 lg:dark:bg-[#101623]">
+        <Link
+          className="github-button"
+          href="https://github.com/rupali-codes/LinksHub"
+          data-icon="octicon-repo-forked"
+          data-size="large"
+          data-show-count="true"
+          aria-label="Fork rupali-codes/LinksHub on GitHub"
+        >
+          Fork
+        </Link>
         <TopBar className="absolute left-8 hidden h-full md:flex" />
         <div className="absolute right-8 flex h-full gap-4">
           <SocialMediaIconsList className="hidden lg:flex" />
@@ -27,6 +51,6 @@ export const Header: FC<{}> = () => {
           </button>
         </div>
       </div>
-    </header>
-  )
-}
+    </div>
+  );
+};
