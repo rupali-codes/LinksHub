a0d679c2e2e0b538762d06cd495883079c3fa0df
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-07-11 20:01:38 +0530

    feat: display buttons in separate line

--- components/SocialMedia/SocialMediaIconsList.tsx
+++ components/SocialMedia/SocialMediaIconsList.tsx
@@ -11,7 +11,7 @@ export const SocialMediaIconsList: FC<{
   const { className, showGithubButtons } = props
 
   return (
-    <ul className={`flex items-center gap-6 ${className}`}>
+    <ul className={`flex flex-wrap gap-6 ${className}`}>
       {!showGithubButtons && (
         <>
           <li className="mr-2 hidden md:block">
@@ -22,64 +22,66 @@ export const SocialMediaIconsList: FC<{
           </li>
         </>
       )}
-      <li>
-        <a
-          title="Link to Discord server (External Link)"
-          className="dark:text-gray-300"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://discord.com/invite/NvK67YnJX5"
-          aria-label="Visit us on Discord"
-        >
-          <IconContext.Provider
-            value={{ className: 'shared-class', size: '24' }}
+      <div className="flex items-center gap-6">
+        <li>
+          <a
+            title="Link to Discord server (External Link)"
+            className="dark:text-gray-300"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://discord.com/invite/NvK67YnJX5"
+            aria-label="Visit us on Discord"
           >
-            <FaDiscord className="hover:text-discord transition duration-300 ease-in-out" />
-          </IconContext.Provider>
-        </a>
-      </li>
-      <li>
-        <a
-          title="Link to Github project (External Link)"
-          className="dark:text-gray-300"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/rupali-codes/LinksHub"
-          aria-label="Visit us on Github"
-        >
-          <IconContext.Provider
-            value={{ className: 'shared-class', size: '24' }}
+            <IconContext.Provider
+              value={{ className: 'shared-class', size: '24' }}
+            >
+              <FaDiscord className="hover:text-discord transition duration-300 ease-in-out" />
+            </IconContext.Provider>
+          </a>
+        </li>
+        <li>
+          <a
+            title="Link to Github project (External Link)"
+            className="dark:text-gray-300"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/rupali-codes/LinksHub"
+            aria-label="Visit us on Github"
           >
-            <FaGithub className="hover:text-github transition duration-300 ease-in-out" />
-          </IconContext.Provider>
-        </a>
-      </li>
-      <li>
-        <a
-          title="Link to Twitter page (External Link)"
-          className="dark:text-gray-300"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://twitter.com/linkshubdotdev"
-          aria-label="Visit us on Twitter"
-        >
-          <IconContext.Provider
-            value={{ className: 'shared-class', size: '24' }}
+            <IconContext.Provider
+              value={{ className: 'shared-class', size: '24' }}
+            >
+              <FaGithub className="hover:text-github transition duration-300 ease-in-out" />
+            </IconContext.Provider>
+          </a>
+        </li>
+        <li>
+          <a
+            title="Link to Twitter page (External Link)"
+            className="dark:text-gray-300"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/linkshubdotdev"
+            aria-label="Visit us on Twitter"
           >
-            <FaTwitter className="hover:text-twitter transition duration-300 ease-in-out" />
-          </IconContext.Provider>
-        </a>
-      </li>
+            <IconContext.Provider
+              value={{ className: 'shared-class', size: '24' }}
+            >
+              <FaTwitter className="hover:text-twitter transition duration-300 ease-in-out" />
+            </IconContext.Provider>
+          </a>
+        </li>
+      </div>
       <li className="flex-1" />
       {showGithubButtons && (
-        <>
+        <div className="flex justify-end">
           <li className="mr-2 hidden md:block">
             <GitHubForkButton repo="rupali-codes/LinksHub" />
           </li>
           <li className="mr-2 hidden md:block">
             <GitHubStarButton repo="rupali-codes/LinksHub" />
           </li>
-        </>
+        </div>
       )}
     </ul>
   )
