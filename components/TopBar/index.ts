282b02d0d093b3d1b2077c8ef81a9439bb8460d4
Harmeet Singh <130365071+harmeetsingh11@users.noreply.github.com>
2023-07-11 12:38:30 +0000

    fix bug: search query overlaps search result

--- components/TopBar/index.ts
+++
@@ -1,1 +0,0 @@
-import { TopBar } from './TopBar'
--- pages/[subcategory]/index.tsx
+++ pages/[subcategory]/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { TopBar } from 'components/TopBar/TopBar'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
 import useFilterDB from 'hooks/useFilterDB'
@@ -27,7 +26,6 @@ const SubCategory = () => {
         <title>{title}</title>
         <meta name="theme-color" content="#202c46" />
       </Head>
-      <TopBar className="shadow-black-500/50 fixed top-[76px] z-30 flex w-full -translate-x-4 items-center bg-gray-100 px-4 pt-6 pb-4 shadow-xl dark:bg-gray-900 md:hidden" />
       <div className="min-h-[calc(100%-68px)] w-full pt-[85px] pb-4 md:min-h-[calc(100%-76px)] md:px-10 md:pt-10">
         {content}
       </div>
