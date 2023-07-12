cc98b40c839f2f2bc1beba6961063b3539906a83
Piyush <piyushkakde79@gmail.com>
2023-07-12 19:02:58 +0530

    Added descriptions of categories.

--- components/TopBar/CategoryDescriptions.ts
+++ components/TopBar/CategoryDescriptions.ts
@@ -5,29 +5,17 @@ type CategoryDescriptions = {
 const categoryDescriptions: CategoryDescriptions = {
   //frontend
   accessibility: 'Accessibility is the concept of whether a product or service can be used by everyone—however they encounter it. Accessibility laws exist to aid people with disabilities, but designers should try to accommodate all potential users in many contexts of use anyway. ',
-
   animations: 'Animation is the state of being full of life and energy, like a room full of excited kids at a birthday party. It is a method of photographing successive drawings, models, or even puppets, to create an illusion of movement in a sequence.',
-
   colors: 'Colors mean the aspect of the appearance of objects and light sources that may be described in terms of hue, lightness, and saturation',
-
   'design inspiration': 'The right kind of inspiration will always help you come up with creative and unique ideas for your projects but sometimes you need some help getting inspired. Luckily, there are a TON of great resources for design inspiration.',
-
   fonts: 'A font is a graphical representation of text that may include a different typeface, point size, weight, color, or design. Fonts are text characters of different types and sizes. They help designers convey interface content intuitively and guide web or mobile app users to interact with the interface smoothly. The term “font” actually refers to a set of graphics that correspond to some or all of the Unicode glyphs',
-
   icons: 'An icon is a symbol or graphic representation on a screen of a program, option, or window. Icons make it easier to recognize and locate items on your computer or features within a program and improve accessibilty and understandability.',
-
   illustrations: 'Illustrations are visual representations of text, concepts or ideas. They can be used to convey complex information in a simple and easy-to-understand way.',
-
   images: 'Images are visual representations of objects or concepts. They can be used to convey information in a simple and easy-to-understand way.',
-
   onlineCodeEditors: 'Online code editors are web-based tools that allow you to write and test code in your browser. They are useful for quickly testing out code snippets or for working on small projects.',
-
   react: 'React is a JavaScript library for building user interfaces. It was developed by Facebook and is used by many popular websites such as Netflix, Airbnb, and Instagram. React allows developers to create reusable UI components and makes it easy to manage the state of an application.',
-
   'themes templates': 'Theme templates are pre-designed website themes that can be customized to fit your needs. They are useful for quickly setting up a website without having to design everything from scratch.',
-
   'ui generator': 'A UI generator is a tool that allows you to create user interfaces without having to write code. Some popular UI generators include Webflow, Figma, and Sketch. ',
-
   videos: 'Videos are visual representations of moving images and sound. They can be used to convey information in a simple and easy-to-understand way.',
   //backend
   architecture: 'Architecture is the art and technique of designing and building structures. It is both the process and the product of sketching, conceiving, planning, designing.',
