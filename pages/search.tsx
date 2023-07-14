c099ba4437146757868d32d0120c2690101006f4
Rupali Haldiya <78981177+rupali-codes@users.noreply.github.com>
2023-07-14 14:43:58 +0530

    Revert "fix bug: search query overlaps search result"

--- pages/search.tsx
+++ pages/search.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 import CardsList from 'components/Cards/CardsList'
+import { TopBar } from 'components/TopBar/TopBar'
 import ComingSoon from 'components/NewIssue/NewIssue'
 
 import useFilterSearch from 'hooks/useFilterSearch'
@@ -35,6 +36,7 @@ const Search = () => {
         <title>{title}</title>
         <meta name="theme-color" content="#202c46" />
       </Head>
+      <TopBar className="shadow-black-500/50 fixed top-[76px] z-30 flex w-full -translate-x-4 items-center bg-gray-100 px-4 pt-6 pb-4 shadow-xl dark:bg-gray-900 md:hidden" />
       <div className="min-h-[calc(100%-68px)] w-full pt-[85px] pb-4 md:min-h-[calc(100%-76px)] md:px-10 md:pt-10">
         {content}
       </div>
