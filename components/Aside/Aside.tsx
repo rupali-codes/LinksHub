9a1fc2e6edb9345297414c27b5659dd4ccad503c
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-07-19 00:22:00 -0700

    fix: warnings due to any and empty props (#1322)

--- components/Aside/Aside.tsx
+++ components/Aside/Aside.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 import { SideNavbarBody } from 'components/SideNavbar/SideNavbarBody'
 
-export const Aside: FC<{}> = () => {
+export const Aside: FC = () => {
   return (
     <aside className="fixed top-[76px] left-0 z-[20] hidden h-screen w-[290px] lg:block">
       <SideNavbarBody />
