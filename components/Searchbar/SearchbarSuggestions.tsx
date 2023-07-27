3e078bd706ecc09a315a711e238c64d91e25d2b9
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2023-07-27 14:46:30 +0530

    merge style changes

--- components/Searchbar/SearchbarSuggestions.tsx
+++ components/Searchbar/SearchbarSuggestions.tsx
@@ -8,14 +8,14 @@ export const SearchbarSuggestions: React.FC<SuggestionsProps> = ({
   onSuggestionClick,
 }) => {
   return (
-    <ul className="absolute z-10 bg-white dark:bg-gray-800 w-full py-2 mt-1 rounded-lg shadow-lg">
+    <ul className="absolute z-10 text-light-primary bg-theme-secondary w-full mt-1 rounded-lg shadow-2xl">
       {suggestions.map((suggestion) => (
         <li
           key={suggestion}
-          className="px-4 py-2 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700"
+          className="px-4 py-2 cursor-pointer hover:bg-[rgba(0,0,0,0.2)] capitalize"
           onClick={() => onSuggestionClick(suggestion)}
         >
-          {suggestion}
+          {suggestion.replace('-', ' ')}
         </li>
       ))}
     </ul>
