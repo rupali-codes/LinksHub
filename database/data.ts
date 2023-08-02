04d1a020c3e1d2bdb67078edc4d9e0d94058298a
Subho Ghosh <93722719+ighoshsubho@users.noreply.github.com>
2023-08-02 20:14:23 +0530

    Updated database/data.ts
    Co-authored-by: Rupali Haldiya <78981177+rupali-codes@users.noreply.github.com>

--- database/data.ts
+++ database/data.ts
@@ -52,7 +52,7 @@ export const sidebarData: ISidebar[] = [
   {
     category: 'devops',
     subcategory: [
-      { name: 'cicd', url: '/cicd', resources: DB.cicd },
+      { name: 'CI/CD', url: '/cicd', resources: DB.cicd },
       { name: 'devopsLifecycle', url: '/devopsLifecycle', resources: DB.devopsLifecycle },
       { name: 'devopsMethodologies', url: '/devopsMethodologies', resources: DB.devopsMethodologies },
       { name: 'docker', url: '/docker', resources: DB.docker },
