b60863c8e2f0cd891fe345cf9dfadcdc305b4e4f
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-03 12:11:11 +0530

    chore: add comments to decribe about parts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,14 +2,17 @@ import { Html, Head, Main, NextScript } from 'next/document'
 import Script from 'next/script'
 
 export default function Document() {
+  const metaDescription =
+    'LinksHub is the ultimate hub of ready-to-use tech resources. Discover free tools and libraries to streamline your development process and build better projects.'
+  const ogImageUrl =
+    'https://res.cloudinary.com/dhnkuonev/image/upload/v1683805184/linkshub_gcahgs.png'
+
   return (
     <Html lang="en">
       <Head>
+        {/* Common Meta Tags */}
         <meta name="title" content="LinksHub" />
-        <meta
-          name="description"
-          content="LinksHub is the ultimate hub of ready-to-use tech resources. Discover free tools and libraries to streamline your development process and build better projects."
-        />
+        <meta name="description" content={metaDescription} />
         <meta
           name="keywords"
           content="LinksHub, developers, free resources, tools, software, libraries, frameworks, applications, websites"
@@ -17,48 +20,41 @@ export default function Document() {
         <meta name="author" content="Rupali Haldiya" />
         <meta name="robots" content="index, follow" />
         <meta name="revisit-after" content="7 days" />
+        <meta name="language" content="English" />
 
-        {/* Should be changed on hosting the website */}
+        {/* Open Graph Meta Tags */}
         <meta property="og:url" content="https://linkshub.dev" />
         <meta property="og:type" content="website" />
         <meta
           property="og:title"
           content="LinksHub: A hub of ready-to-use tech resources"
         />
-        <meta
-          property="og:description"
-          content="LinksHub aims to provide developers with access to a wide range of free resources and tools that they can use in their work."
-        />
-        <meta
-          property="og:image"
-          content="https://res.cloudinary.com/dhnkuonev/image/upload/v1683805184/linkshub_gcahgs.png"
-        />
+        <meta property="og:description" content={metaDescription} />
+        <meta property="og:image" content={ogImageUrl} />
         <meta property="og:site_name" content="LinksHub" />
-        {/* <!-- Twitter --> */}
+
+        {/* Twitter Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://linkshub.dev" />
         <meta
           property="twitter:title"
           content="LinksHub: A hub of ready-to-use tech resources"
         />
-        <meta
-          property="twitter:description"
-          content="LinksHub aims to provide developers with access to a wide range of free resources and tools that they can use in their work."
-        />
-        <meta
-          property="twitter:image"
-          content="https://res.cloudinary.com/dhnkuonev/image/upload/v1683805184/linkshub_gcahgs.png"
-        />
-        <meta name="language" content="English" />
+        <meta property="twitter:description" content={metaDescription} />
+        <meta property="twitter:image" content={ogImageUrl} />
         <meta
           name="twitter:site"
           content="https://twitter.com/linkshubdotdev"
         />
+
+        {/* Discord Meta Tags */}
         <meta property="discord:server" content="1064977356198006805" />
         <meta
           property="discord:invite"
           content="https://discord.com/invite/NvK67YnJX5"
         />
+
+        {/* Google Tag Manager Script */}
         <Script
           id="my-script"
           strategy="lazyOnload"
@@ -72,6 +68,7 @@ export default function Document() {
         />
       </Head>
       <body>
+        {/* Google Tag Manager NoScript */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-PC5DFJG"
@@ -80,9 +77,13 @@ export default function Document() {
             style={{ display: 'none', visibility: 'hidden' }}
           ></iframe>
         </noscript>
+
+        {/* Overlay and Backdrop Roots */}
         <div id="overlay-root"></div>
         <div id="backdrop-root"></div>
         <Main />
+
+        {/* Next.js Script */}
         <NextScript />
       </body>
     </Html>
