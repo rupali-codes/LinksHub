e0667fa024b638b810edc1c7013ef3e12f8f1617
Deepak Singh <deepkushwah824@gmail.com>
2023-08-04 18:51:29 +0530

    maintainer updated

--- data/maintainersData.ts
+++ data/maintainersData.ts
@@ -2,4 +2,5 @@ export const maintainersData = [
   { login: 'rupali-codes', name: 'Rupali Codes' },
   { login: 'CBID2', name: 'CBID2' },
   { login: 'ujjawaltyagii', name: 'Ujjawal Tyagi' },
+  { login: 'k-deepak04', name: 'Deepak Singh' },
 ]
