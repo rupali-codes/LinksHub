8b43ce044c80ae5fe517e560f4774b93d6132e9c
Deepaksingh Kushwah <deepkushwah824@gmail.com>
2023-08-05 15:59:49 +0530

    Delete maintainersData.ts

--- data/maintainersData.ts
+++
@@ -1,6 +0,0 @@
-export const maintainersData = [
-  { login: 'rupali-codes', name: 'Rupali Codes' },
-  { login: 'CBID2', name: 'CBID2' },
-  { login: 'ujjawaltyagii', name: 'Ujjawal Tyagi' },
-  { login: 'k-deepak04', name: 'Deepak Singh' },
-]
