c98f9b00a5aca6df80a53cfc1dc282784c438fc0
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2023-08-05 13:43:55 +0530

    fix: incorrect translate tailwind class into arbitrary class

--- components/CopyToClipboard/CopyToClipboard.tsx
+++ components/CopyToClipboard/CopyToClipboard.tsx
@@ -39,7 +39,7 @@ export const CopyToClipboard = ({ url }: CopyToClipboardProps): JSX.Element => {
         )}
         {isHovering && (
           <p
-            className="bg-theme-secondary text-white text-sm rounded-lg px-3 py-1 cursor-default absolute top-[-2rem] left-[50%] translate-x-3/4 z-10"
+            className="bg-theme-secondary text-white text-sm rounded-lg px-3 py-1 cursor-default absolute top-[-2rem] left-[50%] translate-x-[-75%] z-10"
           >
             {success ? 'Copied!' : 'Copy'}
           </p>
