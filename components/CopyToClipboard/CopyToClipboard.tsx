6055354d744cf4c42362354bb139ba0a715d9703
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2023-08-05 12:33:35 +0530

    feat: add tooltip for haptic feedback after successful clipboard copy

--- components/CopyToClipboard/CopyToClipboard.tsx
+++ components/CopyToClipboard/CopyToClipboard.tsx
@@ -1,5 +1,5 @@
 import useCopyToClipboard from 'hooks/useCopyToClipboard';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaRegCopy, FaCheckSquare } from 'react-icons/fa';
 
 type CopyToClipboardProps = {
@@ -13,21 +13,45 @@ export const CopyToClipboard = ({ url }: CopyToClipboardProps): JSX.Element => {
   function handleCopy() {
     copyToClipboard(url);
     setIsCopied(true);
-    setTimeout(() => setIsCopied(false), 2000); // Reset the icon after 2 seconds
   }
+
+  useEffect(() => {
+    if (isCopied) {
+      const timeout = setTimeout(() => setIsCopied(false), 2000);
+      return () => clearTimeout(timeout);
+    }
+  }, [isCopied]);
 
   return (
     <button
       className="text-theme-primary cursor-pointer bg-transparent border-none p-0"
       onClick={handleCopy}
-      title="Copy link"
-      aria-label={success && isCopied ? 'Link copied' : 'Copy link'}
+      title={success && isCopied ? 'Link copied' : 'Copy link'}
+      aria-label="Copy link to clipboard"
       aria-live="polite"
     >
       {success && isCopied ? (
         <FaCheckSquare size={'1.3rem'} />
       ) : (
         <FaRegCopy size={'1.3rem'} />
+      )}
+      {success && isCopied && (
+        <div
+          className="tooltip"
+          style={{
+            position: 'absolute',
+            top: '-2rem',
+            left: '50%',
+            transform: 'translateX(-50%)',
+            zIndex: 1,
+            backgroundColor: 'rgba(0, 0, 0, 0.8)',
+            color: '#fff',
+            borderRadius: '4px',
+            padding: '4px 8px',
+          }}
+        >
+          Link Copied!
+        </div>
       )}
     </button>
   );
