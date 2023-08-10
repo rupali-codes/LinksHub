63d6a00e8a230abe43e39c812c14a9c9e6947c11
Harmeet Singh <130365071+harmeetsingh11@users.noreply.github.com>
2023-08-10 16:23:50 +0530

    fix bug: text overlap in search result (#1471)
    * fix bug: text overlap in search result
    
    * fix bug: vercel deployment error
    
    * made suggested changes
    
    * fix: code scanning error
    
    * fix: codeql error
    
    * Update search.tsx
    
    * chore: fix deployment
    
    ---------
    
    Co-authored-by: Rupali Haldiya <78981177+rupali-codes@users.noreply.github.com>

--- components/TopBar/TopBar.tsx
+++ components/TopBar/TopBar.tsx
@@ -1,19 +1,38 @@
-import { FC, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { FaSlackHash, FaInfoCircle } from 'react-icons/fa'
 import PopupDesc from 'components/popup/popupCategoryDesc'
 import { ICategoryData } from 'types'
 import categoryDescriptions from './CategoryDescriptions'
+import { useResults } from 'hooks/ResultsContext'
 import { Tooltip } from 'react-tooltip'
 
-export const TopBar: FC<{ className?: string | undefined }> = (props) => {
-  const { className } = props
+interface TopBarProps {
+  className?: string
+  results: number
+}
+
+export const TopBar: FC<TopBarProps> = ({ className }) => {
+  const { results } = useResults()
+  const [isSearchFound, setIsSearchFound] = useState(false)
   const [currentCategory, setCurrentCategory] = useState<ICategoryData | null>(
     null
   )
   const router = useRouter()
   const category = router.asPath.replace('/', '')
   const categoryName = category.split('-').join(' ')
+  const regEx = /[ `!@#$%^&*()_+\=\[\]{};':"\\|,.<>\/?~]/g
+   const cleanedCategory = category
+    .replaceAll(regEx, ' ')
+    .replaceAll('search query ', '')
+
+  useEffect(() => {
+    if (results > 0) {
+      setIsSearchFound(true)
+    } else if (results === 0) {
+      setIsSearchFound(false)
+    }
+  }, [results])
 
   if (router.pathname.length === 1) {
     return null
@@ -33,28 +52,78 @@ export const TopBar: FC<{ className?: string | undefined }> = (props) => {
   }
 
   return (
-    <div
-      className={`flex items-center text-xl dark:text-gray-300 ${className}`}
-    >
-      <FaSlackHash className="mr-2 text-gray-600 dark:text-gray-300" />
-      <span className="text-gray-900 dark:text-gray-100 uppercase">
-        {category.split('-').join(' ')}
-      </span>
-      <button
-          data-tooltip-id="info-tooltip"
-          data-tooltip-content="Description"
-          data-tooltip-place="bottom"
+    <>
+      {regEx.test(category) ? (
+        <div
+          className={`flex items-center text-xl dark:text-gray-300 ${className}`}
         >
-          <FaInfoCircle
-            className="ml-4 mt-2 text-sm cursor-pointer hover:text-theme-primary"
-            onClick={handleCardClick}
+          <FaSlackHash className="mr-2 text-gray-600 dark:text-gray-300" />
+          <span className="flex uppercase text-gray-900 dark:text-gray-100">
+            {isSearchFound
+              ? `${cleanedCategory}`
+              : `No Results Found`}
+          </span>
+          <button
+            data-tooltip-id="info-tooltip"
+            data-tooltip-content="Description"
+            data-tooltip-place="bottom"
+          >
+            <FaInfoCircle
+              className="ml-4 mt-2 text-sm cursor-pointer hover:text-theme-primary"
+              onClick={handleCardClick}
+            />
+          </button>
+          <Tooltip
+            id="info-tooltip"
+            style={{
+              backgroundColor: '#8b5cf6',
+              fontSize: '13px',
+              paddingLeft: '6px',
+              paddingRight: '6px',
+              paddingTop: '2px',
+              paddingBottom: '2px',
+            }}
           />
-        </button>
-        <Tooltip id="info-tooltip" style={{ backgroundColor: '#8b5cf6', fontSize: '13px', paddingLeft: '6px', paddingRight: '6px', paddingTop: '2px', paddingBottom: '2px' }} />
-        <PopupDesc
-          currentCategory={currentCategory}
-          onClose={removeCurrentCard}
-        />
-    </div>
+            <PopupDesc
+              currentCategory={currentCategory}
+              onClose={removeCurrentCard}
+            />
+        </div>
+      ) : (
+        <div
+          className={`flex items-center text-xl dark:text-gray-300 ${className}`}
+        >
+          <FaSlackHash className="mr-2 text-gray-600 dark:text-gray-300" />
+          <span className="flex uppercase text-gray-900 dark:text-gray-100">
+            {category.split('-').join(' ')}
+          </span>
+            <button
+            data-tooltip-id="info-tooltip"
+            data-tooltip-content="Description"
+            data-tooltip-place="bottom"
+          >
+            <FaInfoCircle
+              className="ml-4 mt-2 text-sm cursor-pointer hover:text-theme-primary"
+              onClick={handleCardClick}
+            />
+          </button>
+          <Tooltip
+            id="info-tooltip"
+            style={{
+              backgroundColor: '#8b5cf6',
+              fontSize: '13px',
+              paddingLeft: '6px',
+              paddingRight: '6px',
+              paddingTop: '2px',
+              paddingBottom: '2px',
+            }}
+          />
+            <PopupDesc
+              currentCategory={currentCategory}
+              onClose={removeCurrentCard}
+            />         
+        </div>
+      )}
+    </>
   )
 }
