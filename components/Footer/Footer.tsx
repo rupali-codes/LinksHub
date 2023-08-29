21776906236390feb67ed84a11fa4ac2c74f673d
the-shivam-gupta <109647722+the-shivam-gupta@users.noreply.github.com>
2023-08-29 13:48:27 +0000

    Reduced the fontSize for the small devices and also have added for the medium and large devices.

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -11,7 +11,7 @@ export const Footer: FC = () => {
   } `
 
   return (
-    <footer className="z-10 mb-4 flex w-full items-baseline justify-center rounded-lg py-2 backdrop-blur-md sm:py-3">
+    <footer className="z-10 mb-4 flex w-full items-baseline justify-center rounded-lg py-2 backdrop-blur-md sm:py-3 text-sm md:text-base lg:text-lg xl:text-xl">
       <p className="leading-7 tracking-wide text-center text-black dark:text-theme-primary">
         <span>&copy; {new Date().getFullYear()} LinksHub | </span>
         Developed by{' '}
