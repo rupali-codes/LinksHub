0a3a44ba802a93037fce2b031d1c98dc8f094ce5
Rupali Haldiya <rupali7487@gmail.com>
2023-08-31 12:51:38 +0530

    chore: added subcategory name

--- components/TopBar/TopBar.tsx
+++ components/TopBar/TopBar.tsx
@@ -19,12 +19,20 @@ export const TopBar: FC<TopBarProps> = ({ className }) => {
     null
   )
   const router = useRouter()
-  const category = router.asPath.replace('/', '')
-  const categoryName = category.split('-').join(' ')
+  const category = router.asPath
+  const categoryName = category.split('/')[1].split('-').join(' ')
+  const subcategoryName = category?.split('/')[2]?.split('-').join(' ')
   const regEx = /[ `!@#$%^&*()_+\=\[\]{};':"\\|,.<>\/?~]/g
-   const cleanedCategory = category
-    .replaceAll(regEx, ' ')
-    .replaceAll('search query ', '')
+  let cleanedCategory = ''
+  if (subcategoryName) {
+    cleanedCategory = subcategoryName
+      .replaceAll(regEx, ' ')
+      .replaceAll('search query ', '')
+  } else {
+    cleanedCategory = categoryName
+      .replaceAll(regEx, ' ')
+      .replaceAll('search query ', '')
+  }
 
   useEffect(() => {
     if (results > 0) {
@@ -53,76 +61,78 @@ export const TopBar: FC<TopBarProps> = ({ className }) => {
 
   return (
     <>
-      {regEx.test(category) ? (
-        <div
-          className={`flex items-center text-xl dark:text-gray-300 ${className}`}
-        >
-          <FaSlackHash className="mr-2 text-gray-600 dark:text-gray-300" />
-          <span className="flex uppercase text-gray-900 dark:text-gray-100">
-            {isSearchFound
-              ? `${cleanedCategory}`
-              : `No Results Found`}
-          </span>
-          <button
-            data-tooltip-id="info-tooltip"
-            data-tooltip-content="Description"
-            data-tooltip-place="bottom"
-          >
-            <FaInfoCircle
-              className="ml-4 mt-2 text-sm cursor-pointer hover:text-theme-primary"
-              onClick={handleCardClick}
-            />
-          </button>
-          <Tooltip
-            id="info-tooltip"
-            style={{
-              backgroundColor: '#8b5cf6',
-              fontSize: '13px',
-              paddingLeft: '6px',
-              paddingRight: '6px',
-              paddingTop: '2px',
-              paddingBottom: '2px',
-            }}
-          />
-            <PopupDesc
-              currentCategory={currentCategory}
-              onClose={removeCurrentCard}
-            />
-        </div>
-      ) : (
-        <div
-          className={`flex items-center text-xl dark:text-gray-300 ${className}`}
-        >
-          <FaSlackHash className="mr-2 text-gray-600 dark:text-gray-300" />
-          <span className="flex uppercase text-gray-900 dark:text-gray-100">
-            {category.split('-').join(' ')}
-          </span>
-            <button
-            data-tooltip-id="info-tooltip"
-            data-tooltip-content="Description"
-            data-tooltip-place="bottom"
-          >
-            <FaInfoCircle
-              className="ml-4 mt-2 text-sm cursor-pointer hover:text-theme-primary"
-              onClick={handleCardClick}
-            />
-          </button>
-          <Tooltip
-            id="info-tooltip"
-            style={{
-              backgroundColor: '#8b5cf6',
-              fontSize: '13px',
-              paddingLeft: '6px',
-              paddingRight: '6px',
-              paddingTop: '2px',
-              paddingBottom: '2px',
-            }}
-          />
-            <PopupDesc
-              currentCategory={currentCategory}
-              onClose={removeCurrentCard}
-            />         
-        </div>
+      {subcategoryName && (
+        <>
+          {regEx.test(subcategoryName) ? (
+            <div
+              className={`flex items-center text-xl dark:text-gray-300 ${className}`}
+            >
+              <FaSlackHash className="mr-2 text-gray-600 dark:text-gray-300" />
+              <span className="flex uppercase text-gray-900 dark:text-gray-100">
+                {isSearchFound ? `${cleanedCategory}` : `No Results Found`}
+              </span>
+              <button
+                data-tooltip-id="info-tooltip"
+                data-tooltip-content="Description"
+                data-tooltip-place="bottom"
+              >
+                <FaInfoCircle
+                  className="ml-4 mt-2 text-sm cursor-pointer hover:text-theme-primary"
+                  onClick={handleCardClick}
+                />
+              </button>
+              <Tooltip
+                id="info-tooltip"
+                style={{
+                  backgroundColor: '#8b5cf6',
+                  fontSize: '13px',
+                  paddingLeft: '6px',
+                  paddingRight: '6px',
+                  paddingTop: '2px',
+                  paddingBottom: '2px',
+                }}
+              />
+              <PopupDesc
+                currentCategory={currentCategory}
+                onClose={removeCurrentCard}
+              />
+            </div>
+          ) : (
+            <div
+              className={`flex items-center text-xl dark:text-gray-300 ${className}`}
+            >
+              <FaSlackHash className="mr-2 text-gray-600 dark:text-gray-300" />
+              <span className="flex uppercase text-gray-900 dark:text-gray-100">
+                {subcategoryName}
+              </span>
+              <button
+                data-tooltip-id="info-tooltip"
+                data-tooltip-content="Description"
+                data-tooltip-place="bottom"
+              >
+                <FaInfoCircle
+                  className="ml-4 mt-2 text-sm cursor-pointer hover:text-theme-primary"
+                  onClick={handleCardClick}
+                />
+              </button>
+              <Tooltip
+                id="info-tooltip"
+                style={{
+                  backgroundColor: '#8b5cf6',
+                  fontSize: '13px',
+                  paddingLeft: '6px',
+                  paddingRight: '6px',
+                  paddingTop: '2px',
+                  paddingBottom: '2px',
+                }}
+              />
+              <PopupDesc
+                currentCategory={currentCategory}
+                onClose={removeCurrentCard}
+              />
+            </div>
+          )}
+        </>
       )}
     </>
   )
