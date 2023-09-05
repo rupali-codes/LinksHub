f8250af19282f90c0bc7eb19bb1350ff024ce919
Rupali Haldiya <rupali7487@gmail.com>
2023-09-06 00:20:09 +0530

    chore: fixed warnings

--- pages/[category]/index.tsx
+++ pages/[category]/index.tsx
@@ -19,15 +19,23 @@ const CategoryPage = () => {
   return (
     <section className="flex min-h-[calc(100%-68px)] flex-col">
       <div className=" m-auto items-start gap-2">
-        <div className="m-auto gap-2 flex flex-col items-center justify-center">
-         <p className='text-5xl uppercase font-bold text-theme-secondary dark:text-gray-text'>
-         <span className='text-theme-primary'>-/</span>{category}
-         </p>
-         <p className='text-xl hidden md:block'>Get access to all exclusive <span className='border-b-2 border-theme-primary capitalize text-theme-primary'>{category}</span> resources!</p>
+        <div className="m-auto gap-2 flex flex-col items-center justify-center ">
+          <p className="md:text-4xl text-xl uppercase font-bold text-theme-secondary dark:text-gray-text w-fit mx-auto text-center">
+            <span className="text-theme-primary">-/</span>
+            {category}
+          </p>
+          <p className="text-xl hidden md:block">
+            Get access to all exclusive{' '}
+            <span className="border-b-2 border-theme-primary capitalize text-theme-primary">
+              {category}
+            </span>{' '}
+            resources!
+          </p>
         </div>
         <div className="flex flex-wrap gap-3 mt-6 justify-center items-start lg:w-96 mx-auto max-md:w-96">
           {subcategories.map((subcat, i) => (
             <Link
+              key={i}
               href={`/${category}${subcat.url}`}
               className={`border-2 dark:border-theme-primary rounded-md bg-transparent dark:text-theme-primary text-text-secondary px-3 py-1 text-md capitalize hover:border-dashed hover:text-light-primary shadow-lg`}
             >
