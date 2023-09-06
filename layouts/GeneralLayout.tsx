59c7e75295fa357c44c34eec77f7805101505f0f
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-09-06 15:34:52 -0400

    Top bar distinction feature (#1643)
    * feat: Add visual distinction between top bar and cards (#1422)
    
    * removed shadow from header
    
    * feat: Add visual distinction between top bar and cards (#1422)
    
    * fix: removed bar from header and applied it to GeneralLayouts.tsx for th

--- layouts/GeneralLayout.tsx
+++ layouts/GeneralLayout.tsx
@@ -8,9 +8,12 @@ import { SkipLink } from 'components/SkipLink/SkipLink'
 import { useContext } from 'react'
 import { IContext } from 'types'
 import { GlobalContext } from 'context/GlobalContext'
+import clsx from 'clsx'
+import { usePathname } from 'next/navigation'
 
 const GeneralLayout = ({ children }: { children: ReactNode }) => {
   const { sidebar } = useContext<IContext>(GlobalContext)
+  const pathname = usePathname()
 
   return (
     <>
@@ -26,7 +29,12 @@ const GeneralLayout = ({ children }: { children: ReactNode }) => {
           <Aside />
         </nav>
         <main
-          className="h-full px-4 lg:ml-[290px] lg:w-[calc(100%-290px)]"
+          className={clsx(
+            'h-full px-4 lg:ml-[290px] lg:w-[calc(100%-290px)]',
+
+            pathname != '/' &&
+              'lg:border-t lg:border-t-theme-primary lg:dark:border-t-theme-secondary transition-color ease-in-out duration-200'
+          )}
           id="main"
         >
           {children}
