c6634556b93d713bca27db141f87c0279fb2762a
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-09-10 20:20:44 -0400

    Fix: merge conflicts

--- pages/[category]/[...subcategory].tsx
+++ pages/[category]/[...subcategory].tsx
@@ -7,6 +7,8 @@ import ComingSoon from 'components/NewIssue/NewIssue'
 import { sidebarData } from 'database/data'
 import { GetStaticProps, NextPage } from 'next'
 import { ParsedUrlQuery } from 'querystring'
+import { usePagination } from 'hooks/usePagination'
+import Pagination from 'components/Pagination/Pagination'
 
 interface PageProps {
   category: string
@@ -20,10 +22,14 @@ const SubCategory: NextPage<PageProps> = ({ subcategory }) => {
   const title = `LinksHub - ${
     pageCategory[0].toUpperCase() + pageCategory.slice(1)
   }`
+  const { totalPages, currentPage, startIndex, endIndex, handlePageChange } =
+    usePagination(filterDB.length ? filterDB[0].length : 0)
   let content: JSX.Element[] | JSX.Element
+  let filterData
 
-  if (filterDB.length > 0) {
-    content = <CardsList cards={filterDB[0]} />
+  if (filterDB && filterDB.length > 0) {
+    filterData = filterDB[0].slice(startIndex, endIndex)
+    content = <CardsList cards={filterData} />
   } else {
     content = <ComingSoon />
   }
@@ -93,8 +99,14 @@ const SubCategory: NextPage<PageProps> = ({ subcategory }) => {
         className="shadow-black-500/50 fixed top-[76px] z-30 flex w-full -translate-x-4 items-center bg-gray-100 px-4 pt-6 pb-4 shadow-xl dark:bg-gray-900 md:hidden"
         results={results}
       />
-      <div className="min-h-[calc(100%-68px)] w-full pt-[85px] pb-4 md:min-h-[calc(100%-76px)] md:px-10 md:pt-10">
+      <div className="relative min-h-[calc(100%-68px)] w-full pt-[85px] pb-4 md:min-h-[calc(100%-76px)] md:px-10 md:pt-10">
         {content}
+
+        <Pagination
+          totalPages={totalPages}
+          currentPage={currentPage}
+          handlePageChange={handlePageChange}
+        />
       </div>
     </>
   )
