6bbcb5defd4464ba0385dc7da2be6d7f7da3fa6a
hona09 <mabishalhona@gmail.com>
2023-09-18 11:16:01 +0545

    chore: fix copyright design as required

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -9,9 +9,10 @@ export const Footer: FC = () => {
   return (
     <footer className="z-10 mb-4 flex w-full items-baseline justify-center rounded-lg py-2 backdrop-blur-md text-center px-2 py-2">
       <div className="text-[8px] md:text-[10px] lg:text-sm leading-7 md:tracking-wide text-center text-black dark:text-theme-primary ">
-        <span>&copy; {new Date().getFullYear()} LinksHub </span>
-        <div className="sm: -mt-4 md:-mt-2 lg:mt-0">
-          Developed by{' '}
+        <div>&copy; {new Date().getFullYear()} LinksHub </div>
+        <div className="lg:flex">
+          <div className="sm: -mt-4 mr-1 md:-mt-2 lg:mt-0"> Developed by </div>
+          <div className="sm: -mt-4 md:-mt-2 lg:mt-0"></div>
           <Link
             href="https://rupali-codes.vercel.app/"
             target="_blank"
