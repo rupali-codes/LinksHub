4d756b32b7c23a678293964dd09cd9a1547b1d70
Bishal Hona <67814742+hona9@users.noreply.github.com>
2023-09-19 21:07:13 +0545

    chore: Reduce copyright font-size for small screens (#1668)
    * chore: Reduce copyright font-size for small screens
    
    * chore: Fix footer size and alignment according to need
    
    * chore: fix copyright design as required
    
    * chore: adjust copyright font size

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -7,40 +7,43 @@ export const Footer: FC = () => {
   const isDarkMode = resolvedTheme === 'dark'
 
   return (
-    <footer className="z-10 mb-4 flex w-full items-baseline justify-center rounded-lg py-2 backdrop-blur-md text-sm text-center px-2 py-2">
-      <p className="leading-7 md:tracking-wide text-center text-black dark:text-theme-primary ">
-        <span>&copy; {new Date().getFullYear()} LinksHub | </span>
-        Developed by{' '}
-        <Link
-          href="https://rupali-codes.vercel.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="GitHub Profile of Rupali Haldiya"
-        >
-          <span
-            className={`underline ${
-              isDarkMode ? 'text-light-primary' : 'text-theme-secondary'
-            } `}
+    <footer className="z-10 mb-4 flex w-full items-baseline justify-center rounded-lg py-2 backdrop-blur-md text-center px-2 py-2">
+      <div className="text-sm leading-7 md:tracking-wide text-center text-black dark:text-theme-primary ">
+        <div>&copy; {new Date().getFullYear()} LinksHub </div>
+        <div className="lg:flex">
+          <div className="sm: -mt-2 mr-1 md:-mt-2 lg:mt-0"> Developed by </div>
+          <div className="sm: -mt-2 md:-mt-2 lg:mt-0"></div>
+          <Link
+            href="https://rupali-codes.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub Profile of Rupali Haldiya"
           >
-            Rupali Haldiya
-          </span>
-        </Link>{' '}
-        and{' '}
-        <Link
-          href="/contributors"
-          rel="noopener noreferrer"
-          target="_blank"
-          aria-label="List of Contributors in LinksHub"
-        >
-          <span
-            className={`underline ${
-              isDarkMode ? 'text-light-primary' : 'text-theme-secondary'
-            } `}
+            <span
+              className={`underline mr-1 ${
+                isDarkMode ? 'text-light-primary' : 'text-theme-secondary'
+              } `}
+            >
+              Rupali Haldiya
+            </span>
+          </Link>{' '}
+          and{' '}
+          <Link
+            href="/contributors"
+            rel="noopener noreferrer"
+            target="_blank"
+            aria-label="List of Contributors in LinksHub"
           >
-            Open Source Community
-          </span>
-        </Link>
-      </p>
+            <span
+              className={`underline ml-1 ${
+                isDarkMode ? 'text-light-primary' : 'text-theme-secondary'
+              } `}
+            >
+              Open Source Community
+            </span>
+          </Link>
+        </div>
+      </div>
     </footer>
   )
 }
