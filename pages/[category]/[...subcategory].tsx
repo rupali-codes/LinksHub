3a07e27a85d94790d5a515776efac4d587678bd7
aftabrehan <aftabrehan52422@gmail.com>
2023-10-11 23:44:10 +0500

    Fix Build

--- pages/[category]/[...subcategory].tsx
+++ pages/[category]/[...subcategory].tsx
@@ -18,7 +18,7 @@ interface PageProps {
 interface Params extends ParsedUrlQuery, PageProps {}
 
 const SubCategory: NextPage<PageProps> = ({ subcategory }) => {
-  const { filterDB, results, pageCategory } = useFilterDB(subcategory[0])
+  const { filterDB, pageCategory } = useFilterDB(subcategory[0])
   const title = `LinksHub - ${
     pageCategory[0].toUpperCase() + pageCategory.slice(1)
   }`
@@ -36,7 +36,7 @@ const SubCategory: NextPage<PageProps> = ({ subcategory }) => {
 
   return (
     <>
-      <Head  >
+      <Head>
         <title>{title}</title>
         <meta name="title" content="LinksHub" />
         <meta
@@ -95,13 +95,12 @@ const SubCategory: NextPage<PageProps> = ({ subcategory }) => {
           content="https://discord.com/invite/NvK67YnJX5"
         />
       </Head>
-      <TopBar
-        className="shadow-black-500/50 fixed top-[76px] z-30 flex w-full -translate-x-4 items-center bg-gray-100 px-4 pt-6 pb-4 shadow-xl dark:bg-gray-900 md:hidden"
-        results={results}
-      />
-      <div data-custom="restrict-click-outside" className="relative min-h-[calc(100vh-165px)] w-full pt-[85px] pb-16 md:min-h-[calc(100%-76px)] md:px-10 md:pt-10">
+      <TopBar className="shadow-black-500/50 fixed top-[76px] z-30 flex w-full -translate-x-4 items-center bg-gray-100 px-4 pt-6 pb-4 shadow-xl dark:bg-gray-900 md:hidden" />
+      <div
+        data-custom="restrict-click-outside"
+        className="relative min-h-[calc(100%-68px)] w-full pt-[85px] pb-4 md:min-h-[calc(100%-76px)] md:px-10 md:pt-10"
+      >
         {content}
-
         <Pagination
           totalPages={totalPages}
           currentPage={currentPage}
