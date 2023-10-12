761549bc90a1ad383c2c4416ae685c6b16c0a9a3
Rupali Haldiya <78981177+rupali-codes@users.noreply.github.com>
2023-10-12 13:39:35 +0530

    Revert "fix: fixed the behavior of dropdown list when clicked outside of its scope (#1838)" (#1963)
    This reverts commit b1eec4b9c816a2ddaa4a18fb65277450f6354bd1.

--- components/SideNavbar/SideNavbarCategoryList.tsx
+++ components/SideNavbar/SideNavbarCategoryList.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState, useRef } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import type { SubCategories } from '../../types'
 import { sidebarData } from 'database/data'
 import { SideNavbarCategory } from './SideNavbarCategory'
@@ -10,7 +10,6 @@ export const SideNavbarCategoryList: FC<{
   const categoriesList = getFilteredCategoryList(query)
   const router = useRouter()
   const [category, setCategory] = useState<string | undefined>('')
-  const listRef = useRef<HTMLUListElement | null>(null)
 
   useEffect(() => {
     const cat: string | undefined = router.query.category as string | undefined
@@ -29,13 +28,10 @@ export const SideNavbarCategoryList: FC<{
   }
 
   return (
-    <ul ref={listRef} className="mt-2 flex flex-col justify-center px-4 pb-24">
-      {/* Reset the open/close states of categories. This makes sure that
-      changes to the toggle state don't reflect when a new query is submitted */}
+    <ul className="mt-2 flex flex-col justify-center px-4 pb-24">
       <React.Fragment key={query}>
         {categoriesList.map((categoryData) => (
           <SideNavbarCategory
-            listRef={listRef}
             key={categoryData.category}
             categoryData={categoryData}
             expand={query.length > 0 || category === categoryData.category}
