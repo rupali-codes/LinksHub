ac722137e7f4a21cf6e37abe5952aed8c991c25a
Pratham Mahajan <111378171+Killer2OP@users.noreply.github.com>
2023-10-22 19:13:31 +0530

    Update index.ts

--- database/index.ts
+++ database/index.ts
@@ -121,10 +121,8 @@ export {default as dsaArticles} from './data_structures/dsa-articles.json'
 export {default as dsaTutorials} from './data_structures/dsa-tutorials.json'
 export { default as dsaArticles } from './data_structures/dsa-articles.json'
 export { default as dsaTutorials } from './data_structures/dsa-tutorials.json'
-
 // Block Chain
 export {default as smartContracts} from './BlockChain/smart-contracts.json'
 export {default as truffle} from './BlockChain/truffle.json'
 export {default as hardhat} from './BlockChain/hardhat.json'
 export {default as ethers} from './BlockChain/ethers.json'
-
