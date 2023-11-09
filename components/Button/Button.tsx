0e03b20b691bfc32b2c75505bed55127052ed75c
aftabrehan <aftabrehan52422@gmail.com>
2023-11-09 10:06:02 +0500

    Minor Adjustment

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -19,30 +19,26 @@ const Button: FC<Props> = ({
   variant = 'primary',
   className,
 }) => {
-  const wrapperCls = {
+  const styles = {
     primary: '', // @todo: add styles when using these button types in ResourcesCards
     secondary: '',
-    pale: 'bg-slate-100 hover:bg-violet-50 dark:bg-white dark:bg-opacity-10 dark:hover:bg-opacity-20',
-  }[variant]
-
-  const textCls = {
-    primary: '',
-    secondary: '',
-    pale: 'text-slate-500 dark:text-violet-50',
+    pale: 'bg-slate-100 hover:bg-violet-50 dark:bg-white dark:bg-opacity-10 dark:hover:bg-opacity-20 text-slate-500 dark:text-violet-50',
   }[variant]
 
   const cls = clsx(
     'flex justify-center items-center px-4 py-3 gap-1.5 rounded-lg transition-colors',
-    wrapperCls,
+    styles,
     className
   )
 
   const content = () => (
     <>
-      {icon && <span className="flex items-center justify-center">{icon}</span>}
-      <span className={clsx('text-base font-medium font-urbanist', textCls)}>
-        {label}
-      </span>
+      {icon && (
+        <span className="flex items-center justify-center text-base font-medium font-urbanist">
+          {icon}
+        </span>
+      )}
+      <span>{label}</span>
     </>
   )
 
