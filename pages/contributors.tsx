f30b8873b808202864ffa85ebfd9a8ed8e304039
Anmol Baranwal <anmolbaranwal119@gmail.com>
2023-11-21 00:40:39 +0530

    chore: update color palette as per figma file

--- pages/contributors.tsx
+++ pages/contributors.tsx
@@ -2,14 +2,12 @@ import { FC, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import type { GetStaticProps } from 'next'
-// import { useTheme } from 'next-themes'
 import {
   maintainersData,
   Maintainers,
   dummyContributors,
   Contributor,
 } from '../data/maintainersData'
-// import { useState } from 'react'
 import {
   FaArrowRight,
   FaGithub,
@@ -84,9 +82,6 @@ const ContributorsPage: FC<{ contributors: Contributor[] }> = ({
     rel: 'noopener noreferrer',
   }
 
-  // const { resolvedTheme } = useTheme()
-  // const isDarkMode = resolvedTheme === 'dark'
-
   const iconsComponents: { [key: string]: JSX.Element } = {
     GitHub: <FaGithub />,
     Twitter: <FaXTwitter />,
@@ -106,51 +101,51 @@ const ContributorsPage: FC<{ contributors: Contributor[] }> = ({
   const getDarkBgColor = (id: number, check?: string) => {
     if (id === 0) {
       if (check === 'bg') {
-        return 'bg-[#FFD36226] dark:bg-[#3D3749]' // themecolor
+        return 'bg-[#EF6086] bg-opacity-5 dark:bg-opacity-10 dark:bg-[#EF6086]'
       } else if (check === 'text') {
         return 'text-[#EF6086]'
       } else if (check === 'role') {
-        return 'bg-[#EF608633]'
+        return 'bg-[#EF60861A] bg-opacity-10 dark:bg-[#EF6086] dark:bg-opacity-20 border dark:border-none border-[#EF6086] border-opacity-50'
       } else if (check === 'hover') {
-        return 'dark:hover:bg-[#EF608633] hover:text-[#EF6086]' // roleBg
+        return 'hover:bg-[#EF6086] hover:bg-opacity-5 dark:hover:bg-[#EF6086] dark:hover:bg-opacity-20 hover:text-[#EF6086]'
       } else if (check === 'border') {
-        return 'dark:border-[#EF608633] border-[#EF6086]' // dark:roleBg, txtTheme
+        return 'dark:border-[#EF6086] dark:border-opacity-20 border-[#EF6086]'
       }
     } else if (id === 1) {
       if (check === 'bg') {
-        return 'bg-[#e0e0e0] dark:bg-[#e0e0e0]' // themecolor
+        return 'bg-[#e0e0e0] dark:bg-[#e0e0e0]'
       } else if (check === 'text') {
         return 'text-[#101623]'
       } else if (check === 'role') {
         return 'bg-[#d5d5d5]'
       } else if (check === 'hover') {
-        return 'dark:hover:bg-[#e0e0e0] hover:text-[#101623]' // roleBg
+        return 'dark:hover:bg-[#e0e0e0] hover:text-[#101623]'
       } else if (check === 'border') {
-        return 'dark:border-[#101623] border-[#101623]' // dark:roleBg, txtTheme
+        return 'dark:border-[#101623] border-[#101623]'
       }
     } else if (id === 2) {
       if (check === 'bg') {
-        return 'bg-[#101623] dark:bg-[#101623]' // themecolor
+        return 'bg-[#101623] dark:bg-[#101623]'
       } else if (check === 'text') {
         return 'text-[#101623]'
       } else if (check === 'role') {
         return 'bg-[#d5d5d5]'
       } else if (check === 'hover') {
-        return 'dark:hover:bg-[#e0e0e0] hover:text-[#101623]' // roleBg
+        return 'dark:hover:bg-[#e0e0e0] hover:text-[#101623]'
       } else if (check === 'border') {
-        return 'dark:border-[#101623] border-[#101623]' // dark:roleBg, txtTheme
+        return 'dark:border-[#101623] border-[#101623]'
       }
     } else {
       if (check === 'bg') {
-        return 'bg-[#9655A033] dark:bg-[#9655A033]' // themecolor
+        return 'bg-[#9655A0] bg-opacity-5 dark:bg-opacity-10 dark:bg-[#9655A0]'
       } else if (check === 'text') {
-        return 'text-[#E88DF6]'
+        return 'text-[#E672F9]'
       } else if (check === 'role') {
-        return 'bg-[#9655A080]'
+        return 'bg-[#9655A0] bg-opacity-10 dark:bg-[#9655A0] dark:bg-opacity-50 border dark:border-none border-[#9655A0] border-opacity-50'
       } else if (check === 'hover') {
-        return 'dark:hover:bg-[#a0a0a0] hover:text-[#E88DF6]' // roleBg
+        return 'hover:bg-[#9655A0] hover:bg-opacity-5 dark:hover:bg-[#9655A0] dark:hover:bg-opacity-20 hover:text-[#E672F9]'
       } else if (check === 'border') {
-        return 'dark:border-[#9655A080] border-[#E88DF6]' // dark:roleBg, txtTheme
+        return 'dark:border-[#9655A0] dark:border-opacity-20 border-[#9655A0]'
       }
     }
   }
@@ -158,15 +153,15 @@ const ContributorsPage: FC<{ contributors: Contributor[] }> = ({
   return (
     <div className="mx-4">
       <div>
-        <h2 className="text-2xl text-violet-600 dark:text-white tracking-wide pb-2 mt-14 lg:mt-2">
+        <h2 className="text-2xl text-gray-600 dark:text-white tracking-wide pb-2 mt-14 lg:mt-2">
           Our Team
         </h2>
-        <h4 className="text-[#A6ABBF]">
+        <h4 className="text-[#A1A1A9]">
           Meet Our Team, Passionate About Open Source and Making LinksHub an
           Open Source Success Story
         </h4>
       </div>
-      <div className="maintainers-our-team grid sm:grid-cols-1  xl:grid-cols-2 gap-5 mt-8">
+      <div className="maintainers-our-team grid sm:grid-cols-1 xl:grid-cols-2 gap-5 mt-8">
         {Maintainers.map((maintainer, id) => (
           <div
             key={id}
@@ -258,16 +253,16 @@ const ContributorsPage: FC<{ contributors: Contributor[] }> = ({
               key={contributor.id}
               className="bg-gray-100 rounded-3xl p-4 border border-dashed border-violet-500 dark:border-[#BDBDBD40] shadow-lg dark:bg-[#293242] dark:text-gray-300 dark:shadow-sm flex flex-col hover:scale-105 transition-transform duration-300 cursor-pointer m-1"
             >
-              <div className="bg-gray-200 dark:bg-[#9F87FF1A] rounded-xl">
+              <div className="bg-[#714EFF] bg-opacity-5 dark:bg-[#9F87FF] dark:bg-opacity-10 rounded-xl">
                 <div className="flex justify-center image-wrapper pt-4">
                   <Image
                     src={contributor.avatar_url}
                     alt={contributor.login}
                     width={110}
                     height={110}
-                    className="rounded-full mb-4 border-2 border-violet-500 dark:border-violet-400 transition-transform duration-300 hover:scale-105 hover:border-dotted m-2"
+                    className="rounded-full mb-4 border-2 border-violet-500 dark:border-violet-400 dark:border-opacity-50 transition-transform duration-300 hover:scale-105 hover:border-dotted m-2"
                   />
-                  <div className="bg-[#9F87FF] text-violet-200 text-xs tracking-wide py-1 px-2 rounded-full absolute top-2 right-2">
+                  <div className="bg-[#714EFF] bg-opacity-10 text-[#714EFF] dark:text-[#9F87FF] dark:bg-[#9F87FF] dark:bg-opacity-20 text-xs tracking-wide py-1 px-2 rounded-full absolute top-2 right-2 border border-[#714EFF] border-opacity-20 dark:border-none">
                     Developer
                   </div>
                 </div>
@@ -325,13 +320,13 @@ const ContributorsPage: FC<{ contributors: Contributor[] }> = ({
             </div>
           ))}
       </div>
-      <div className="bg-[#293242] flex justify-between flex-col md:flex-row rounded-xl py-6 px-4 my-10">
+      <div className="bg-white dark:bg-[#293242] flex justify-between flex-col md:flex-row rounded-xl py-6 px-4 my-10">
         <div className="flex space-x-4">
           <div className="pl-2 pr-3">
             <FaTrophy className="text-2xl text-[#FBD449]" />{' '}
           </div>
           <div className="flex flex-col">
-            <div className="text-white text-lg pb-1">
+            <div className="text-[#293242] dark:text-white text-lg pb-1">
               Join our awesome team!
             </div>
             <div className="text-gray-400 pr-2 md:pr-4">
@@ -353,7 +348,7 @@ const ContributorsPage: FC<{ contributors: Contributor[] }> = ({
       </div>
       <div className="flex justify-center items-center mb-8">
         <button
-          className="bg-[#293242] hover:bg-[#465369] text-gray-300 w-36 py-4 px-6 rounded-xl text-center cursor-pointer duration-300 transition-all"
+          className="bg-[#EBE5FF] dark:bg-[#293242] hover:bg-[#d0c8eb] dark:hover:bg-[#384355] text-[#714EFF] dark:text-gray-300 w-36 py-4 px-6 rounded-xl text-center cursor-pointer duration-300 transition-all"
           onClick={() => setDisplayedContributors(displayedContributors + 9)}
         >
           See More
