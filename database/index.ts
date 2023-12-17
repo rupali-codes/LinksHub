b9aec54241e3045a3981a25015063b62619de678
Esteban BARON <94709417+EstebanBAR0N@users.noreply.github.com>
2023-12-17 16:22:25 +0100

    feat: add 'IaC' sub-category with 'Terraform' content under 'devops' category (#2208)
    Signed-off-by: Esteban Baron <estebanbaron.pro@gmail.com>

--- database/index.ts
+++ database/index.ts
@@ -34,6 +34,7 @@ export { default as docker } from './devops/docker.json'
 export { default as jenkins } from './devops/jenkins.json'
 export { default as kubernetes } from './devops/kubernetes.json'
 export { default as microservices } from './devops/microservices.json'
+export { default as iac } from './devops/iac.json'
 
 // languages
 export { default as javascript } from './languages/javascript.json'
