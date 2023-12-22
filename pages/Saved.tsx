49306032f35053831738c380c5e64892accf6a27
Vidip Ghosh <vidip2001@gmail.com>
2023-12-22 13:23:06 +0530

    fix: bug in sidebar links

--- pages/Saved.tsx
+++ pages/Saved.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { FaArrowRight } from "react-icons/fa";
 import Link from 'next/link';
 
-const Saved = () => {
+const saved = () => {
   return (
     <>
       <div>
@@ -12,11 +12,11 @@ const Saved = () => {
       </div>
       <div className='flex h-screen justify-center items-center flex-col'>
         <Image src={'/SaveRemove.png'} width={120} height={120} alt={''} />
-        <h1 className='text-center mt-5 text-lg font-medium'>Your collection is empty! Start saving items for later reference.</h1>
+        <h1 className='text-center sm:text-center md:text-center mt-5 text-lg font-medium'>Your collection is empty! Start saving items for later reference.</h1>
         <Link className='mt-5' href={'/'}><button className='bg-[#714EFF] text-white flex p-3 rounded-lg items-center'>Explore now<FaArrowRight/></button></Link>
       </div>
     </>
   )
 }
 
-export default Saved
+export default saved
