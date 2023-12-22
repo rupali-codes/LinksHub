cdbb38cfc593a77a7afd8e5c50b77c1995ded4de
Vidip Ghosh <vidip2001@gmail.com>
2023-12-22 12:46:58 +0530

    feat: saved resources page

--- pages/saved.tsx
+++ pages/saved.tsx
@@ -11,7 +11,7 @@ const Saved = () => {
         <p className='text-[#A6ABBF]'>Your saved resources, ready when you are.</p>
       </div>
       <div className='flex h-screen justify-center items-center flex-col'>
-        <Image src={'/save-remove.png'} width={120} height={120} alt={''} />
+        <Image src={'/SaveRemove.png'} width={120} height={120} alt={''} />
         <h1 className='text-center mt-5 text-lg font-medium'>Your collection is empty! Start saving items for later reference.</h1>
         <Link className='mt-5' href={'/'}><button className='bg-[#714EFF] text-white flex p-3 rounded-lg items-center'>Explore now<FaArrowRight/></button></Link>
       </div>
