a64f78790574510e0b3f3c0dc7b1260aac6895b5
Vidip Ghosh <vidip2001@gmail.com>
2023-12-29 10:42:02 +0530

    fix: giving space between texts

--- pages/saved.tsx
+++ pages/saved.tsx
@@ -3,15 +3,15 @@ import Image from 'next/image'
 import { FaArrowRight } from "react-icons/fa";
 import Link from 'next/link';
 
-const saved = () => {
+const Saved = () => {
   return (
     <>
-      <div>
+      <div className="p-8">
         <h1 className='text-2xl font-medium'>Saved</h1>
         <p className='text-[#A6ABBF]'>Your saved resources, ready when you are.</p>
       </div>
       <div className='flex h-screen justify-center items-center flex-col'>
-        <Image src={'/SaveRemove.png'} width={120} height={120} alt={''} />
+        <Image src="/SaveRemove.png" width={120} height={120} alt={''} />
         <h1 className='text-center sm:text-center md:text-center mt-5 text-lg font-medium'>Your collection is empty! Start saving items for later reference.</h1>
         <Link className='mt-5' href={'/'}><button className='bg-[#714EFF] text-white flex p-3 rounded-lg items-center'>Explore now<FaArrowRight/></button></Link>
       </div>
@@ -19,4 +19,4 @@ const saved = () => {
   )
 }
 
-export default saved
+export default Saved
