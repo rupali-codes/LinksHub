8f7bda6560ec49c40aec4ee7d9b282123580e1ae
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2024-01-07 22:02:26 -0800

    feat: add new design for Our Team Section (#2218)
    * chore: remove hacktoberfest checkmark
    
    * feat: adding Aftab to Codeowners file (#2167)
    
    * chore: Add Swift Sub Category (#2160)
    
    * feat: Add Swift Sub Category
    
    * Fix small correction
    
    * Update database/languages/swift.json
    
    Co-authored-by: Christine Belzie <105683440+CBID2@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Christine Belzie <105683440+CBID2@users.noreply.github.com>
    
    * [ADDED]: BlockChain Category (#2044)
    
    * [ADDED]: BlockChain Category
    
    * Changes done
    
    * Update index.ts
    
    * Update index.ts
    
    * Changes made
    
    * Update CategoryDescriptions.ts
    
    * chore: add initial structure
    
    * chore: add join team and below part
    
    * chore: Added a new Ui library Url (#2159)
    
    * chore: Add Easings in animations subcategory (#2179)
    
    * chore: add iconsax icon (#2121)
    
    * feat: add iconsax icon
    
    * chore: update description
    
    Co-authored-by: Rupali Haldiya <78981177+rupali-codes@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
    Co-authored-by: Rupali Haldiya <78981177+rupali-codes@users.noreply.github.com>
    
    * Update SideNavbar.tsx (#2116)
    
    * chore: add maintainers data for our team section
    
    * chore: update structure of data for efficiency
    
    * chore: separate data and add initial structure
    
    * chore: add dual mode styling and improve maintainers card
    
    * chore: add responsive styling
    
    * chore: refactor the below links for maintainers card
    
    * chore: add color palette with choice of usage
    
    * Added a Podcast resources (#2135)
    
    * Added 2 new podcasts
    
    * Added Overpowered.AI podcast
    
    * Added new link to podcasts
    
    * fix: json structure
    
    * chore: update description
    
    Co-authored-by: Christine Belzie <105683440+CBID2@users.noreply.github.com>
    
    * chore: remove whitespace
    
    Co-authored-by: Aftab Rehan <workwithaftabrehan@gmail.com>
    
    ---------
    
    Co-authored-by: Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
    Co-authored-by: Christine Belzie <105683440+CBID2@users.noreply.github.com>
    Co-authored-by: Aftab Rehan <workwithaftabrehan@gmail.com>
    
    * chore: added new resources to ui-libraries (#2180)
    
    * UI libraries in fronted added
    
    * added description for the UI libraries
    
    * description update for materailui , antdesign, chakraui
    
    * removed os tutorials
    
    * Added official documents for Go programming language (#2154)
    
    * Added new course
    
    * Added new document
    
    * feat: add new structure for maintainers section
    
    * chore: fix styling for empty twitter username
    
    * feat: implement see more cta functionality
    
    * chore: revert tailwindcss version
    
    * chore: update color palette as per figma file
    
    * feat: add new color theme of maintainers
    
    * chore: improve code structure
    
    * chore: change label tag of Aftan to maintainer
    
    with slight color changes in maintainer section
    
    * chore: fix syntax error due to resolving merge conflict
    
    * feat: fixed general layout to full viewport height
    
    ---------
    
    Co-authored-by: Rupali Haldiya <rupali7487@gmail.com>
    Co-authored-by: Christine Belzie <105683440+CBID2@users.noreply.github.com>
    Co-authored-by: Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
    Co-authored-by: Pratham Mahajan <111378171+Killer2OP@users.noreply.github.com>
    Co-authored-by: Alvis07 <148678038+Alvis07@users.noreply.github.com>
    Co-authored-by: Mrical Singhal <60258119+mrical@users.noreply.github.com>
    Co-authored-by: Jessy Hanifiah <72622302+jeeehaan@users.noreply.github.com>
    Co-authored-by: Rupali Haldiya <78981177+rupali-codes@users.noreply.github.com>
    Co-authored-by: Harshavardhan Bajoria <62978274+HVbajoria@users.noreply.github.com>
    Co-authored-by: Aftab Rehan <workwithaftabrehan@gmail.com>
    Co-authored-by: Hritik Bhattacharya <44370586+hritikbhattacharya@users.noreply.github.com>
    Co-authored-by: dalvishruti14 <120911455+dalvishruti14@users.noreply.github.com>

--- data/maintainersData.ts
+++ data/maintainersData.ts
@@ -1,7 +1,164 @@
 export const maintainersData = [
   { login: 'rupali-codes', name: 'Rupali Haldiya' },
   { login: 'CBID2', name: 'Christine Belzie' },
-  { login: 'ujjawaltyagii', name: 'Ujjawal Tyagi' },
-  { login: 'k-deepak04', name: 'Deepak Singh' },
   { login: 'Anmol-Baranwal', name: 'Anmol Baranwal' },
+  { login: 'aftabrehan', name: 'Aftab Rehan' },
+]
+
+export interface MaintainersDataInterface {
+  name: string
+  avatarUrl: string
+  login: string
+  designation: string
+  role: string
+  firstLink: string
+  firstTxt: string
+  secondLink: string
+  secondTxt: string
+  thirdLink: string
+  thirdTxt: string
+}
+
+export const Maintainers: MaintainersDataInterface[] = [
+  {
+    name: 'Rupali Haldiya',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/78981177?v=4',
+    login: 'rupali-codes',
+    designation: 'FullStack Developer',
+    role: 'Founder',
+    firstLink: 'https://github.com/sponsors/rupali-codes',
+    firstTxt: 'Sponsor',
+    secondLink: 'https://github.com/rupali-codes',
+    secondTxt: 'GitHub',
+    thirdLink: 'https://twitter.com/rupali_codes',
+    thirdTxt: 'Twitter',
+  },
+  {
+    name: 'Anmol Baranwal',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/74038190?v=4',
+    login: 'Anmol-Baranwal',
+    designation: 'Software Developer',
+    role: 'Maintainer',
+    firstLink: 'https://www.linkedin.com/in/Anmol-Baranwal',
+    firstTxt: 'LinkedIn',
+    secondLink: 'https://github.com/Anmol-Baranwal',
+    secondTxt: 'GitHub',
+    thirdLink: 'https://twitter.com/Anmol_Codes',
+    thirdTxt: 'Twitter',
+  },
+  {
+    name: 'Christine Belzie',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/105683440?v=4',
+    login: 'CBID2',
+    designation: 'Technical Writer',
+    role: 'Maintainer',
+    firstLink: 'https://github.com/sponsors/CBID2',
+    firstTxt: 'Sponsor',
+    secondLink: 'https://www.linkedin.com/in/christinebelzie',
+    secondTxt: 'LinkedIn',
+    thirdLink: 'https://twitter.com/CodesChrissy',
+    thirdTxt: 'Twitter',
+  },
+  {
+    name: 'Aftab Rehan',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/93012310?v=4',
+    login: 'aftabrehan',
+    designation: 'UI/UX Designer',
+    role: 'Maintainer',
+    firstLink: 'https://aftabrehan.com',
+    firstTxt: 'Website',
+    secondLink: 'https://github.com/aftabrehan',
+    secondTxt: 'GitHub',
+    thirdLink: 'https://twitter.com/aftabrehan_',
+    thirdTxt: 'Twitter',
+  },
+]
+
+export interface Contributor {
+  twitter_username?: string
+  id: number
+  avatar_url: string
+  name: string
+  login: string
+  contributions: number
+}
+
+export const dummyContributors: Contributor[] = [
+  {
+    id: 1,
+    avatar_url: 'https://avatars.githubusercontent.com/u/74038190?v=4',
+    name: 'John Doe',
+    login: 'johndoe',
+    contributions: 15,
+  },
+  {
+    id: 2,
+    avatar_url: 'https://avatars.githubusercontent.com/u/74038190?v=4',
+    name: 'Jane Smith',
+    login: 'janesmith',
+    contributions: 10,
+  },
+  {
+    id: 3,
+    avatar_url: 'https://avatars.githubusercontent.com/u/74038190?v=4',
+    name: 'Bob Johnson',
+    login: 'bobjohnson',
+    contributions: 5,
+  },
+  {
+    id: 4,
+    avatar_url: 'https://avatars.githubusercontent.com/u/74038190?v=4',
+    name: 'Bob Johnson',
+    login: 'bobjohnson',
+    contributions: 5,
+  },
+  {
+    id: 5,
+    avatar_url: 'https://avatars.githubusercontent.com/u/74038190?v=4',
+    name: 'Bob Johnson',
+    login: 'bobjohnson',
+    contributions: 5,
+  },
+  {
+    id: 6,
+    avatar_url: 'https://avatars.githubusercontent.com/u/74038190?v=4',
+    name: 'Bob Johnson',
+    login: 'bobjohnson',
+    contributions: 5,
+  },
+  {
+    id: 7,
+    avatar_url: 'https://avatars.githubusercontent.com/u/74038190?v=4',
+    name: 'Bob Johnson',
+    login: 'bobjohnson',
+    contributions: 5,
+  },
+  {
+    id: 8,
+    avatar_url: 'https://avatars.githubusercontent.com/u/74038190?v=4',
+    name: 'Bob Johnson',
+    login: 'bobjohnson',
+    contributions: 5,
+  },
+  {
+    id: 9,
+    avatar_url: 'https://avatars.githubusercontent.com/u/74038190?v=4',
+    name: 'Bob Johnson',
+    login: 'bobjohnson',
+    contributions: 5,
+  },
+  {
+    id: 10,
+    avatar_url: 'https://avatars.githubusercontent.com/u/74038190?v=4',
+    name: 'Bob Johnson',
+    login: 'bobjohnson',
+    contributions: 5,
+  },
+  {
+    id: 11,
+    avatar_url: 'https://avatars.githubusercontent.com/u/74038190?v=4',
+    name: 'Bob Johnson',
+    login: 'bobjohnson',
+    contributions: 5,
+  },
 ]
