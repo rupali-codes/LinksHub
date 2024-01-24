5ff09b3f2220aa8dece1f63e8c21aadbfbca114b
Anmol Baranwal <anmolbaranwal119@gmail.com>
2024-01-24 23:50:31 +0530

    fix: popup on clicking card & read more to reveal full info

--- components/Cards/CardsListItem.tsx
+++ components/Cards/CardsListItem.tsx
@@ -3,10 +3,13 @@ import { Card } from './Card'
 import type { IData } from 'types'
 
 const CardsListItem: FC<{ data: IData; onClick: () => void }> = (props) => {
-  const { data } = props
+  const { data, onClick } = props
 
   return (
-    <li className="w-full transition-all duration-100 ease-in hover:scale-[1.02] ">
+    <li
+      className="w-full transition-all duration-100 ease-in hover:scale-[1.02] cursor-pointer"
+      onClick={onClick}
+    >
       <Card data={data} />
     </li>
   )
