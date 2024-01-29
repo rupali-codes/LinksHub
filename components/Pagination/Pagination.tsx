eadaa03003025f63932ada0d8af3c8cfae68d0ea
Med Frezghi <55899220+mfrezghi@users.noreply.github.com>
2024-01-29 04:54:29 -0500

    chore: align pagination to bottom (#2239)

--- components/Pagination/Pagination.tsx
+++ components/Pagination/Pagination.tsx
@@ -7,6 +7,7 @@ type PaginationProps = {
   totalPages: number[] | null
   currentPage: number
   handlePageChange: (page: number) => void
+  numberOfCards: number
 }
 
 export default function Pagination({
@@ -14,6 +15,7 @@ export default function Pagination({
   totalPages,
   currentPage,
   handlePageChange,
+  numberOfCards,
 }: PaginationProps) {
   const { resolvedTheme } = useTheme()
   const isDarkMode = resolvedTheme === 'dark'
@@ -29,61 +31,72 @@ export default function Pagination({
     scrollToTop()
   }, [currentPage])
 
+  const remainderOfCards = numberOfCards % 9
+  
   return (
     <>
       {totalPages && totalPages.length > 1 && (
-        <div
-          className={clsx(
-            toporbottom == true
-              ? 'w-full z-20 flex lg:w-full items-center justify-center absolute bottom-2 right-0'
-              : 'z-20 flex w-full lg:w-full items-center justify-end absolute top-0 right-0'
-          )}
-        >
+        <div>
           <div
-            className={clsx(
-              'flex items-center',
-              toporbottom == true ? 'px-6 py-1 gap-4' : 'gap-2'
+          className={(            
+            currentPage == totalPages.length && remainderOfCards <= 3
+              ? 'p-20'
+              : 'w-full z-20 flex lg:w-full items-center justify-center absolute bottom-2 right-0'
+          )}
+          />
+          <div
+            className={clsx(            
+              toporbottom == true
+                ? 'w-full z-20 flex lg:w-full items-center justify-center absolute bottom-2 right-0'
+                : 'z-20 flex w-full lg:w-full items-center justify-end absolute top-0 right-0'
             )}
           >
-            <button
+            <div
               className={clsx(
-                'flex items-center justify-center text-[#8b5cf6]',
-                isDarkMode ? 'hover:text-white' : 'hover:text-black',
-                'disabled:text-gray-400'
+                'flex items-center',
+                toporbottom == true ? 'px-6 py-1 gap-4' : 'gap-2'
               )}
-              onClick={() => changePage(currentPage - 1)}
-              disabled={currentPage === 1}
             >
-              Prev
-            </button>
-            {totalPages &&
-              totalPages.map((page, index) => (
-                <button
-                  key={index}
-                  className={clsx(
-                    'flex items-center justify-center rounded-md hover:bg-[#8b5cf6] hover:text-white px-2',
-                    currentPage === page
-                      ? 'bg-[#8b5cf6] text-white'
-                      : isDarkMode
-                      ? 'text-light'
-                      : 'text-theme-secondary'
-                  )}
-                  onClick={() => changePage(page)}
-                >
-                  {page}
-                </button>
-              ))}
-            <button
-              className={clsx(
-                'flex items-center justify-center text-[#8b5cf6]',
-                isDarkMode ? 'hover:text-white' : 'hover:text-black',
-                'disabled:text-gray-400'
-              )}
-              onClick={() => changePage(currentPage + 1)}
-              disabled={currentPage === totalPages.length}
-            >
-              Next
-            </button>
+              <button
+                className={clsx(
+                  'flex items-center justify-center text-[#8b5cf6]',
+                  isDarkMode ? 'hover:text-white' : 'hover:text-black',
+                  'disabled:text-gray-400'
+                )}
+                onClick={() => changePage(currentPage - 1)}
+                disabled={currentPage === 1}
+              >
+                Prev
+              </button>
+              {totalPages &&
+                totalPages.map((page, index) => (
+                  <button
+                    key={index}
+                    className={clsx(
+                      'flex items-center justify-center rounded-md hover:bg-[#8b5cf6] hover:text-white px-2',
+                      currentPage === page
+                        ? 'bg-[#8b5cf6] text-white'
+                        : isDarkMode
+                        ? 'text-light'
+                        : 'text-theme-secondary'
+                    )}
+                    onClick={() => changePage(page)}
+                  >
+                    {page}
+                  </button>
+                ))}
+              <button
+                className={clsx(
+                  'flex items-center justify-center text-[#8b5cf6]',
+                  isDarkMode ? 'hover:text-white' : 'hover:text-black',
+                  'disabled:text-gray-400'
+                )}
+                onClick={() => changePage(currentPage + 1)}
+                disabled={currentPage === totalPages.length}
+              >
+                Next
+              </button>
+            </div>
           </div>
         </div>
       )}
