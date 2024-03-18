caef7f27322b5b6d3f80aaa8210413217396776d
bottlewook <bwlimtony@gmail.com>
2024-03-18 22:34:26 +0900

    rename: modify path

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,13 +11,13 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* Favicon */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/assets/icons/favicon/android-chrome-192x192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/assets/icons/favicon/android-chrome-512x512.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicon/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/favicon/favicon-32x32.png" />
-        <link rel="shortcut icon" href="/assets/icons/favicon/favicon.ico" />
-        <link rel="manifest" href="/assets/icons/favicon/site.webmanifest" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/favicon/android-chrome-512x512.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
 
         {/* Common Meta Tags */}
         <meta name="title" content="LinksHub" />
