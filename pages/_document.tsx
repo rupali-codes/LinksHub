9c24a86ab050529de146e6ea0c5020c7b5173f0d
bottlewook <bwlimtony@gmail.com>
2024-03-18 22:10:04 +0900

    feat: setup favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,15 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Favicon */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/favicon_io/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/favicon_io/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/icons/favicon_io/android-chrome-512x512.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon_io/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon_io/favicon-32x32.png" />
+        <link rel="shortcut icon" href="/icons/favicon_io/favicon.ico" />
+        <link rel="manifest" href="/icons/favicon_io/site.webmanifest" />
+
         {/* Common Meta Tags */}
         <meta name="title" content="LinksHub" />
         <meta name="description" content={metaDescription} />
