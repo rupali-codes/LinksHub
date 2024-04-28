74cf00d7431054e5de4964f22ddfa3d2a9c0e0c2
Rupali Haldiya <78981177+rupali-codes@users.noreply.github.com>
2024-04-28 15:44:34 +0530

    chore: add design description

--- components/TopBar/CategoryDescriptions.ts
+++ components/TopBar/CategoryDescriptions.ts
@@ -130,7 +130,7 @@ const categoryDescriptions: CategoryDescriptions = {
     'Natural Language Processing is a branch of artificial intelligence that focuses on the interaction between computers and human language. It involves the development of algorithms and models that enable machines to understand and generate human language, allowing them to perform tasks like language translation an more. ',
 
   // AI Tools
-  editors: 
+  design: 
   'Harness the power of AI-driven editing tools to refine your writing effortlessly. From grammar and style suggestions to readability enhancements, these editors ensure your content shines with clarity and precision',
   chatbots:
   'Experience seamless interactions and instant assistance with AI chatbots. Whether it\'s answering queries, providing support, or automating tasks, these intelligent bots deliver personalized responses to enhance user experiences.',
