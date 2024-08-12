4c73b7e2ba4ab90dcbb73d84f9590b18d1e713a8
Dragbos <145892326+drakeRAGE@users.noreply.github.com>
2024-08-12 14:15:32 +0530

    Update Header.tsx

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,19 +1,20 @@
-import { FC } from 'react'
-import Link from 'next/link'
-import clsx from 'clsx'
-import { useRouter } from 'next/router'
 
-import { ThemeToggler } from 'components/ThemeToggler/themeToggler'
-import Button from 'components/Button'
+import { FC } from 'react';
+import Link from 'next/link';
+import clsx from 'clsx';
+import { useRouter } from 'next/router';
 
-import Logo from 'assets/logo.svg'
-import { Icons } from 'components/icons'
+import { ThemeToggler } from 'components/ThemeToggler/themeToggler';
+import Button from 'components/Button';
+
+import Logo from 'assets/logo.svg';
+import { Icons } from 'components/icons';
 
 export const Header: FC = () => {
-  const router = useRouter()
-
+  const router = useRouter();
+  console.log(router.asPath);
   const iconClass =
-    'text-zinc-600 group-hover:text-primary dark:text-zinc-400 dark:group-hover:text-slate-300 h-6 w-6'
+    'text-zinc-600 group-hover:text-primary dark:text-zinc-400 dark:group-hover:text-slate-300 h-6 w-6';
   const socialIcons = [
     {
       icon: <Icons.X className={iconClass} />,
@@ -40,10 +41,10 @@ export const Header: FC = () => {
       ariaLabel: 'Explore on GitHub',
       showOnMobile: true,
     },
-  ]
+  ];
 
-  const inActiveIconCls = 'stroke-gray-400'
-  const activeIconCls = 'text-primary dark:text-white'
+  const inActiveIconCls = 'stroke-gray-400';
+  const activeIconCls = 'text-primary dark:text-white';
 
   const navLinks = [
     {
@@ -59,33 +60,28 @@ export const Header: FC = () => {
       label: 'Team',
       href: '/contributors',
       isDisabled: false,
-    }
-  ]
+    },
+  ];
 
   const renderLinks = () =>
-    navLinks.map(({ inActiveIcon, activeIcon, label, href }, i) => {
-      const checkRoute = (val: string) => router.asPath.startsWith(val)
-
-      const isHomeActive = !checkRoute('/saved') && !checkRoute('/contributors')
-      const isUrlMatched = checkRoute(href)
-      const isActive = label === 'Home' ? isHomeActive : isUrlMatched
-
-      return (
-          <li key={i}>
-            <a
-              href={href}
-              className={`hover:bg-slate-100 hover:bg-opacity-50 dark:hover:bg-zinc-400 dark:hover:bg-opacity-10 flex items-center justify-start p-2 gap-2 text-base font-medium leading-5 rounded-xl ${
-                isActive ? 'text-primary dark:text-white' : 'text-gray-text'
-              }`}
-            >
-              <span className="flex items-center justify-center" title={label}>
-                {isActive ? activeIcon : inActiveIcon}
-              </span>
-              <span>{label}</span>
-            </a>
-          </li>
-      )
-    })
+    navLinks.map(({ inActiveIcon, activeIcon, label, href }, i) => (
+      <li key={i}>
+        <a
+          href={href}
+          className={clsx(
+            'hover:bg-slate-100 hover:bg-opacity-50 dark:hover:bg-zinc-400 dark:hover:bg-opacity-10 flex items-center justify-start p-2 gap-2 text-base font-medium leading-5 rounded-xl',
+            router.asPath === href
+              ? 'text-primary dark:text-white'
+              : 'text-gray-text'
+          )}
+        >
+          <span className="flex items-center justify-center" title={label}>
+            {router.asPath === href ? activeIcon : inActiveIcon}
+          </span>
+          <span>{label}</span>
+        </a>
+      </li>
+    ));
 
   return (
     <header className="fixed top-0 left-0 z-30 row-start-1 row-end-2 h-[76px] w-screen flex items-center justify-between px-6 bg-white dark:bg-slate-800 shadow-header dark:shadow-none">
@@ -130,5 +126,5 @@ export const Header: FC = () => {
         />
       </div>
     </header>
-  )
-}
+  );
+};
